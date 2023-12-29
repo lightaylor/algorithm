994f2e8082889c1172dcafaa833f026fe47d7ac8
TaeGyeong115 <115taegyeong@gmail.com>
2023-12-29 21:14:34 +0900

    add Day23 Babbling

---
+++ 1. Introduction to Coding Tests/Day23/Babbling.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] words = {"aya", "ye", "woo", "ma"};
+        String temp = "";
+
+        for (int i = 0; i < babbling.length; i++) {
+            temp = babbling[i];
+            for (int j = 0; j < words.length; j++) {
+                if (babbling[i].contains(words[j])) {
+                    temp = temp.replaceFirst(words[j], "");
+                }
+            }
+
+            if (temp.equals("")) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
