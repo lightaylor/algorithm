3c8581a48cedfc9b4c0047a247c308623dd690f2
TaeGyeong115 <115taegyeong@gmail.com>
2023-12-29 23:43:29 +0900

    add 3. Unfinished Player

---
+++ 2. Coding Test High Score Kit/Unfinished Player.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        String result = participant[participant.length-1];
+        int j = 0;
+
+        for(int i = 0; i < completion.length; i++) {
+            if(!completion[i].equals(participant[j])) {
+                result = participant[j];
+                j++;
+            }
+            j++;
+        }
+        return result;
+    }
+}
