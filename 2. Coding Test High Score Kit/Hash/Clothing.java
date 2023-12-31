f4ff0f5a79052c3cbd1b71ba46108b19e323fc63
TaeGyeong <115taegyeong@gmail.com>
2023-12-31 19:12:50 +0900

    add hash Clothing

---
+++ 2. Coding Test High Score Kit/Hash/Clothing.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for(String[] key : clothes) {
+            if(!map.containsKey(key[1])) {
+                map.put(key[1], 2);
+            } else {
+                map.put(key[1], map.get(key[1]) + 1);
+            }
+        }
+
+        for(String k : map.keySet()) {
+            answer = answer * map.get(k);
+        }
+
+        return answer - 1;
+    }
+}
