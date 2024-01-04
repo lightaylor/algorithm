cd7a6d8500adc2bb5517a5d81b5f014113fa1b00
TaeGyeong <115taegyeong@gmail.com>
2024-01-04 22:14:01 +0900

    add Feature Development

---
+++ 2. Coding Test High Score Kit/Stack Queue/Feature Development.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        Queue q = new LinkedList();
+
+        for(int i = 0; i < progresses.length; i++) {
+            q.add((int)Math.ceil((double)(100 - progresses[i]) / speeds[i]));
+        }
+
+        while(!q.isEmpty()) {
+            answer = compare(answer, (int)q.poll(), q, 1);
+        }
+
+        return answer;
+    }
+
+    public int[] compare(int[] answer, int init, Queue q, int counter) {
+        if(q.peek() != null && init >= (int)q.peek()) {
+            q.poll();
+            answer = compare(answer, init, q, counter + 1);
+        } else {
+            answer = Arrays.copyOf(answer, answer.length+1);
+            answer[answer.length - 1] = counter;
+            return answer;
+        }
+        return answer;
+    }
+}
