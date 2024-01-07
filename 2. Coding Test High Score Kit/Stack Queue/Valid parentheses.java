a39c7d73347a32afd34870805895c69e3b27daa4
TaeGyeong <115taegyeong@gmail.com>
2024-01-07 20:16:17 +0900

    add Valid parentheses

---
+++ 2. Coding Test High Score Kit/Stack Queue/Valid parentheses.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        char[] cArray = s.toCharArray();
+        stack.push(cArray[0]);
+
+        for (int i = 1; i < cArray.length; i++) {
+            if (!stack.empty() && cArray[i] == ')' && stack.peek() ==  '(') {
+                stack.pop();
+            } else {
+                stack.push(cArray[i]);
+            }
+        }
+
+        return stack.empty();
+    }
+}
