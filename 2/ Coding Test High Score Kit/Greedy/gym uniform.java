a487dc91a73d1ee8821790a8ebc0454fe792620b
TaeGyeong <115taegyeong@gmail.com>
2024-01-08 21:41:14 +0900

    add greedy gym uniform

---
+++ 2/ Coding Test High Score Kit/Greedy/gym uniform.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] total = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            total[i] += 1;
+        }
+
+        for(int i : reserve) {
+            total[i-1] += 1;
+        }
+
+        for(int i : lost) {
+            total[i-1] -= 1;
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(total[i] == 0) {
+                if(i > 0 && total[i-1] == 2) {
+                    total[i-1] -= 1;
+                    ++answer;
+                } else if(i+1 < n && total[i+1] == 2) {
+                    total[i+1] -= 1;
+                    ++answer;
+                }
+            } else {
+                ++answer;
+            }
+        }
+
+        return answer;
+    }
+}
