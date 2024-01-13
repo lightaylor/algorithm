d92062e6a76c9dbd440a78c4218429786afbd037
TaeGyeong <115taegyeong@gmail.com>
2024-01-13 21:13:53 +0900

    add sum of digits

---
+++ Practice Exercise/sum of digits.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        char[] c = String.valueOf(n).toCharArray();
+
+        for (int i = 0; i < c.length; i++) {
+            answer += Character.getNumericValue(c[i]);
+        }
+
+        return answer;
+    }
+}
