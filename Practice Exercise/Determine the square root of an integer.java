1aa453083f932aed518d3439ef8bedc512eb6c3a
TaeGyeong <115taegyeong@gmail.com>
2024-01-14 21:28:47 +0900

    add Determine the square root of an integer

---
+++ Practice Exercise/Determine the square root of an integer.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+class Solution {
+    public long solution(long n) {
+        long temp = (long)Math.sqrt(n);
+
+        if (temp * temp == n) {
+            return (temp + 1) * (temp + 1);
+        }
+        return -1;
+    }
+}
