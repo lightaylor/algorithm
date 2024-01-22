69f4fb9e5b0462af94a601050749e37be5298b9a
TaeGyeong <115taegyeong@gmail.com>
2024-01-22 21:15:22 +0900

    add Maximum and Minimum Values

---
+++ Practice Exercise/Maximum and Minimum Values.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] c = s.split(" ");
+        int[] temp = new int[c.length];
+        for (int i = 0; i < c.length; i++) {
+            temp[i] = Integer.parseInt(c[i]);
+        }
+        Arrays.sort(temp);
+        return temp[0] + " " + temp[temp.length - 1];
+    }
+}
