6736c811a8f95c093d83a113522f7c703c3527c9
TaeGyeong <115taegyeong@gmail.com>
2024-01-22 21:05:14 +0900

    add Handling Strings Basic

---
+++ Practice Exercise/Handling Strings Basic.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        char[] c = s.toCharArray();
+
+        for (int i = 0; i < c.length; i++) {
+            if ((int)c[i] > 64 || !(c.length == 4 || c.length == 6)) {
+                answer = false;
+            }
+        }
+
+        return answer;
+    }
+}
