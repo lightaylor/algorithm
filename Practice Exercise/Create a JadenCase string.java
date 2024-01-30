cb3fbfe7d166644c1e3319702dc37922b8c623b3
TaeGyeong115 <115taegyeong@gmail.com>
2024-01-30 14:37:00 +0900

    add Create a JadenCase string

---
+++ Practice Exercise/Create a JadenCase string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        char[] c = s.toCharArray();
+        c[0] = Character.toUpperCase(c[0]);
+
+        for(int i = 1; i < c.length; i++) {
+            if(c[i - 1] == ' ') {
+                c[i] = Character.toUpperCase(c[i]);
+            } else {
+                c[i] = Character.toLowerCase(c[i]);
+            }
+        }
+
+        return String.valueOf(c);
+    }
+}
