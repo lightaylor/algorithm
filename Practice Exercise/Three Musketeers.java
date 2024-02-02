5307b815d9707ef625d67fe6ae240736aea9b3ca
TaeGyeong115 <115taegyeong@gmail.com>
2024-02-02 11:46:31 +0900

    fix Three Musketeers

--- Practice Exercise/Three Musketeers.java
+++ Practice Exercise/Three Musketeers.java
@@ -3,22 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        int left = 0;
-        int right = number.length - 1;
-        Arrays.sort(number);
 
-        for (int i = 0; i < number.length - 2; i++) {
-            left = i+1;
-            right = number.length - 1;
-            while(left < right) {
-                if (number[right] + number[i] + number[left] == 0) {
-                    answer++;
-                    left++;
-                    right--;
-                } else if (number[right] + number[i] + number[left] > 0) {
-                    right--;
-                } else {
-                    left++;
+        for(int i = 0; i < number.length; i++) {
+            for(int j = i+1; j < number.length; j++) {
+                for(int k = j+1; k < number.length; k++) {
+                    answer += number[i] + number[j] + number[k] == 0 ? 1 : 0;
+
                 }
             }
         }
