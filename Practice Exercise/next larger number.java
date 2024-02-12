cc5db1cf125c359bf682ee8c6da0764548ca3e47
TaeGyeong <115taegyeong@gmail.com>
2024-02-12 19:41:45 +0900

    add next larger number

---
+++ Practice Exercise/next larger number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        String binary = Integer.toBinaryString(n);
+        int oneCount = binary.length() - binary.replace("1", "").length();
+        int answerCount = 0;
+
+        while (oneCount != answerCount) {
+            n++;
+            binary = Integer.toBinaryString(n);
+            answerCount = binary.length() - binary.replace("1", "").length();
+        }
+
+        return n;
+    }
+}
