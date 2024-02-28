028ad807d0c005187bada03ac7d64ef2f2a56ada
TaeGyeong115 <115taegyeong@gmail.com>
2024-02-28 11:09:19 +0900

    feat: add deck of cards

---
+++ Practice Exercise/deck of cards.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int c1 = 0;
+        int c2 = 0;
+
+        for (int i = 0; i < goal.length; i++) {
+            if (c1 < cards1.length && goal[i].equals(cards1[c1])) {
+                c1++;
+            } else if (c2 < cards2.length && goal[i].equals(cards2[c2])) {
+                c2++;
+            } else {
+                return "No";
+            }
+        }
+
+        return "Yes";
+    }
+}
