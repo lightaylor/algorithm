3b0950402d5be90c25d049b89cf5033c4b2364ba
TaeGyeong <115taegyeong@gmail.com>
2024-03-01 23:24:42 +0900

    fix: add condition

--- Complete Search/carpet.java
+++ Complete Search/carpet.java
@@ -4,10 +4,12 @@ class Solution {
         int total = brown + yellow;
         int temp = total;
 
-        for(int i = 3; i < total; i++) {
-            if (total % i == 0 &&  (total/i + i) < temp) {
-                answer[0] = total/i > i ? total/i : i;
-                answer[1] = total/i > i ? i : total/i;
+        for(int i = 3; i < total-3; i++) {
+            int min = total/i > i ? total/i : i;
+            int max = total/i > i ? i : total/i;
+            if (total % i == 0 && (total / i + i) < temp && (min-2)*(max-2) == yellow) {
+                answer[0] = min;
+                answer[1] = max;
                 temp = total/i + i;
             }
         }
