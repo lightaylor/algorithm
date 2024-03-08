854532d302ea3f85840bf9fc75fc0f81c2067838
TaeGyeong115 <115taegyeong@gmail.com>
2024-03-08 15:06:27 +0900

    feat: add Cutting an n^2 array

---
+++ Monthly Code Challenge Season 3/Cutting an n^2 array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int s = (int)(right - left);
+        int[] answer = new int[s + 1];
+        int a = 0;
+        int b = 0;
+
+        for(int i = 0; i < answer.length; i++) {
+            a = (int)((left + (long)i) % (long)n) + 1;
+            b = (int)((left + (long)i) / (long)n) + 1;
+            answer[i] = a < b ? b : a;
+        }
+
+        return answer;
+    }
+}
