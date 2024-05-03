feea09f2bfa7eac101b4c404b27e2ed1bf54a711
Taylor <115taegyeong@gmail.com>
2024-05-03 16:49:52 +0900

    fix: long -> biginteger

--- baekjoon/test_15841/Main.java
+++ baekjoon/test_15841/Main.java
@@ -1,13 +1,14 @@
 package baekjoon.test_15841;
 
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        long[] dp = {0, 0, 1};
+        BigInteger[] dp = {new BigInteger("0"), new BigInteger("0"), new BigInteger("1")};
         String input = br.readLine();
 
         for(int i = 1; i <= 490; i++) {
@@ -16,9 +17,10 @@ public class Main {
                 bw.write("Hour " + i + ": " + dp[2] + " cow(s) affected\n");
                 input = br.readLine();
             }
+            System.out.println(dp[2]);
             dp[0] = dp[1];
             dp[1] = dp[2];
-            dp[2] = dp[0] + dp[1];
+            dp[2] = dp[0].add(dp[1]);
         }
         bw.close();
     }
