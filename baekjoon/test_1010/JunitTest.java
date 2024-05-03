8510aebcaeb5840a5a72f851de21fae75d5b1c6a
Taylor <115taegyeong@gmail.com>
2024-05-03 19:17:04 +0900

    fix: bug

--- baekjoon/test_1010/JunitTest.java
+++ baekjoon/test_1010/JunitTest.java
@@ -2,31 +2,28 @@ package baekjoon.test_1010;
 
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class JunitTest {
     @Test
-    public void test_1() {
+    public void test_1() throws IOException {
         String input = "3\n2 2\n1 5\n13 29";
-        String expectedOutput = "1\n5\n67863915";
+        String expectedOutput = "1\n5\n67863915\n";
 
         assertEquals(expectedOutput, test(input));
     }
 
     @Test
-    public void test_2() {
+    public void test_2() throws IOException {
         String input = "3\n2 3\n2 4\n2 5";
-        String expectedOutput = "3\n6\n10";
+        String expectedOutput = "3\n6\n10\n";
 
         assertEquals(expectedOutput, test(input));
     }
 
-    public String test(String input) {
+    public String test(String input) throws IOException {
         InputStream stdin = System.in;
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
