d356c37ec4160fbd595fa98da19c53c7638202ac
Taylor <115taegyeong@gmail.com>
2024-05-06 11:48:39 +0900

    refactor: dp

--- baekjoon/test_26529/Main.java
+++ baekjoon/test_26529/Main.java
@@ -1,24 +1,31 @@
 package baekjoon.test_26529;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] dp = {0, 0, 1};
+        int input;
         int count = Integer.parseInt(br.readLine());
-        String input = br.readLine();
+        Long[] dp = {0L, 0L, 1L};
+        List<Long> list = new ArrayList<>();
+        list.add(1L);
+
 
         for(int i = 0; i <= 45; i++) {
-            if(input.equals(String.valueOf(i))) {
-                bw.write(dp[2] + "\n");
-                input = br.readLine();
-            }
             dp[0] = dp[1];
             dp[1] = dp[2];
             dp[2] = dp[0] + dp[1];
+            list.add(dp[2]);
+        }
+
+        for(int j = 0; j < count; j++) {
+            input = Integer.parseInt(br.readLine());
+            bw.write(list.get(input) + "\n");
         }
         bw.close();
     }
