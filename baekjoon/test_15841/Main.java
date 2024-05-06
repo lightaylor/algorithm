83ee0ee87cc38dc42889e8cb8afdfcb2d58a2ff7
Taylor <115taegyeong@gmail.com>
2024-05-06 12:05:55 +0900

    refactor: code

--- baekjoon/test_15841/Main.java
+++ baekjoon/test_15841/Main.java
@@ -2,26 +2,31 @@ package baekjoon.test_15841;
 
 import java.io.*;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int input = Integer.parseInt(br.readLine());
         BigInteger[] dp = {new BigInteger("0"), new BigInteger("0"), new BigInteger("1")};
-        String input = br.readLine();
+        List<BigInteger> list = new ArrayList<>();
+        list.add(new BigInteger("1"));
 
         for(int i = 1; i <= 490; i++) {
-            if(input.equals("-1")) break;
-            else if(input.equals(String.valueOf(i))) {
-                bw.write("Hour " + i + ": " + dp[2] + " cow(s) affected\n");
-                input = br.readLine();
-            }
-            System.out.println(dp[2]);
             dp[0] = dp[1];
             dp[1] = dp[2];
             dp[2] = dp[0].add(dp[1]);
+            list.add(dp[2]);
         }
+
+        while(input > -1) {
+            bw.write("Hour " + input + ": " + list.get(input - 1) + " cow(s) affected\n");
+            input = Integer.parseInt(br.readLine());
+        }
+
         bw.close();
     }
 }
