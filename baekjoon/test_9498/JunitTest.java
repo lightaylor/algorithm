2509c03818264a3082a1957099b119d4ad8d9e5e
Taylor <115taegyeong@gmail.com>
2024-05-08 22:30:16 +0900

    feat: add test_9498

---
+++ baekjoon/test_9498/JunitTest.java
@@ -0,0 +1,35 @@
+package baekjoon.test_9498;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static java.lang.System.*;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "100";
+        String expectedOutput = "A";
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    public String test(String input) throws IOException {
+        setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        setOut(new PrintStream(stdout));
+
+        Main.main(new String[0]);
+
+        setIn(in);
+        setOut(new PrintStream(out));
+
+        return stdout.toString();
+    }
+}
---
+++ baekjoon/test_9498/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.test_9498;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.valueOf(br.readLine());
+
+        switch(input / 10) {
+            case 10:
+                bw.write("A");
+                break;
+            case 9:
+                bw.write("A");
+                break;
+            case 8:
+                bw.write("B");
+                break;
+            case 7:
+                bw.write("C");
+                break;
+            case 6:
+                bw.write("D");
+                break;
+            default:
+                bw.write("F");
+        }
+
+        bw.close();
+    }
+}
