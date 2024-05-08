66a07a1e31a05b76dcfbb06999ddb7779dcdf91e
Taylor <115taegyeong@gmail.com>
2024-05-08 22:38:38 +0900

    feat: add test_2739

---
+++ baekjoon/test_2739/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.test_2739;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.valueOf(br.readLine());
+
+        for(int i = 1; i < 10; i++) {
+            bw.write(n + " * " + i + " = " + i * n + "\n");
+        }
+
+        bw.close();
+    }
+}
