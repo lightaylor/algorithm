06890386b153d09c2d7d62252b9e5757323ef95f
Taylor <115taegyeong@gmail.com>
2024-05-11 16:19:12 +0900

    feat: add test_10172

---
+++ baekjoon/test_10172/Main.java
@@ -0,0 +1,18 @@
+package test_10172;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+        bw.close();
+    }
+}
