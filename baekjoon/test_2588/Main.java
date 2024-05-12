79d841c9d93ea14a3c81b038e3a8e29af20b2035
Taylor <115taegyeong@gmail.com>
2024-05-12 16:07:17 +0900

    feat: add test_2588

---
+++ baekjoon/test_2588/Main.java
@@ -0,0 +1,20 @@
+package test_2588;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        bw.write(a * (b % 10) + "\n");
+        bw.write(a * ((b % 100) - (b % 10)) / 10 + "\n");
+        bw.write(a * ((b % 1000) - (b % 100)) / 100 + "\n");
+        bw.write(String.valueOf(a * b));
+
+        bw.close();
+    }
+}
