20fbbca6e2c3416e3c39fe344151af44f9c31172
Taylor <115taegyeong@gmail.com>
2024-05-13 21:33:43 +0900

    feat: add test_14681

---
+++ baekjoon/test_14681/JunitTest.java
@@ -0,0 +1,43 @@
+package test_14681;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static java.lang.System.*;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "12\n5";
+        String expectedOutput = "1";
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "9\n-13";
+        String expectedOutput = "4";
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    public String test(String input) throws IOException {
+        setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        setOut(new PrintStream(stdout));
+
+        Main.main(new String[0]);
+
+        setIn(in);
+        setOut(new PrintStream(out));
+
+        return stdout.toString();
+    }
+}
