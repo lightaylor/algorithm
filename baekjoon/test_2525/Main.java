bde8e10bcb6506a26b7c20b20a64f6b92a9f619f
Taylor <115taegyeong@gmail.com>
2024-05-16 12:45:34 +0900

    feat: add test_2525

---
+++ baekjoon/test_2525/Main.java
@@ -0,0 +1,27 @@
+package test_2525;
+
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ParseException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String time = br.readLine();
+        int add = Integer.parseInt(br.readLine());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("H m");
+        Date date = sdf.parse(time);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MINUTE, add);
+
+        bw.write(sdf.format(cal.getTime()));
+        bw.close();
+    }
+}
