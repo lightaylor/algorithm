bcc0b09cf801994c85183f26d1f4d08a07b62f94
Taylor <115taegyeong@gmail.com>
2024-05-16 12:15:36 +0900

    feat: add test_11382

---
+++ baekjoon/test_11382/Main.java
@@ -0,0 +1,16 @@
+package test_11382;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long[] arr = Stream.of(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+
+        bw.write(String.valueOf(arr[0] + arr[1] + arr[2]));
+        bw.close();
+    }
+}
