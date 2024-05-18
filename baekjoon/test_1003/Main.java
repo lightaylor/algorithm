16c7e2bdc3021f67ab70b239a2e4d17b99f15faa
Taylor <115taegyeong@gmail.com>
2024-05-19 00:43:21 +0900

    feat: add test_1003

---
+++ baekjoon/test_1003/Main.java
@@ -0,0 +1,27 @@
+package test_1003;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        int[][] dp = new int[41][2];
+        dp[0] = new int[]{1, 0};
+        dp[1] = new int[]{0, 1};
+
+        for (int i = 2; i < 41; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-2][0];
+            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        }
+
+        for (int i = 0; i < count; i++) {
+            int a = Integer.parseInt(br.readLine());
+            bw.write(dp[a][0] + " " + dp[a][1] + "\n");
+        }
+
+        bw.close();
+    }
+}
