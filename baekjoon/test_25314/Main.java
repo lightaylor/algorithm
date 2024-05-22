2b766d9bb549388b9522c1a3a1ffeeca68056eb0
Taylor <115taegyeong@gmail.com>
2024-05-22 20:34:48 +0900

    feat: add test_25314

---
+++ baekjoon/test_25314/Main.java
@@ -0,0 +1,19 @@
+package test_25314;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n/4; i++) {
+            bw.write("long ");
+        }
+
+        bw.write("int");
+        bw.close();
+    }
+}
