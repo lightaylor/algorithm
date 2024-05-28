00ce546e3570db2786d1ebdeba8d63bee33f2cfa
Taylor <115taegyeong@gmail.com>
2024-05-28 23:08:49 +0900

    fix: add empty check

--- baekjoon/test_1152/Main.java
+++ baekjoon/test_1152/Main.java
@@ -7,8 +7,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] n = br.readLine().split(" ");
-        bw.write(String.valueOf(n.length));
+        String input = br.readLine().trim();
+
+        if (input.isEmpty()) {
+            bw.write("0");
+        } else {
+            String[] n = input.split("\\s+");
+            bw.write(String.valueOf(n.length));
+        }
         bw.close();
     }
 }
