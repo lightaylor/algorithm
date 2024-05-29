2185b810fc98dae01aff7c9301815cb1044c21ac
Taylor <115taegyeong@gmail.com>
2024-05-30 00:01:03 +0900

    feat: add test_1157

---
+++ baekjoon/test_1157/Main.java
@@ -0,0 +1,27 @@
+package test_1157;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine().toUpperCase();
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < input.length(); i++) {
+            if (!map.containsKey(Character.toString(input.charAt(i)))) {
+                map.put(String.valueOf(input.charAt(i)), input.length() - input.replace(String.valueOf(input.charAt(i)), "").length());
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+
+        if (list.size() > 1 && Objects.equals(list.get(list.size() - 1).getValue(), list.get(list.size() - 2).getValue())) bw.write("?");
+        else bw.write(list.get(list.size() - 1).getKey());
+        bw.close();
+    }
+}
