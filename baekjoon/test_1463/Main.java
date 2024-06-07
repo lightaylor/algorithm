8c1cbd8696b3e257c60e25f78297c97852f1cda0
lightaylor <115taegyeong@gmail.com>
2024-06-07 21:59:52 +0900

    fix: test_1463

--- baekjoon/test_1463/Main.java
+++ baekjoon/test_1463/Main.java
@@ -7,16 +7,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int input = Integer.valueOf(br.readLine());
+        int input = Integer.parseInt(br.readLine());
         int[] arr = new int[input + 1];
-        arr[0] = arr[1] = 0;
-        arr[2] = 1;
 
-        for (int i = 3; i <= input; i++) {
-            if (i % 6 == 0) arr[i] = Math.min(arr[i/3] + arr[i/2], arr[i - 1]);
-            else if (i % 3 == 0) arr[i] = Math.min(arr[i/3] + 1, arr[i - 1] + 1);
-            else if (i % 2 == 0) arr[i] = Math.min(arr[i/2] + 1, arr[i - 1] + 1);
-            else arr[i] = arr[i - 1] + 1;
+        for (int i = 2; i <= input; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0) arr[i] = Math.min(arr[i], arr[i/2] + 1);
+            if (i % 3 == 0) arr[i] = Math.min(arr[i], arr[i/3] + 1);
         }
 
         bw.write(String.valueOf(arr[input]));
