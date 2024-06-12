c866d9cc5161dd2cb965062c72acd5e89e1de92c
lightaylor <115taegyeong@gmail.com>
2024-06-12 10:32:17 +0900

    feat: add test_24264

---
+++ baekjoon/test_24264/Main.java
@@ -0,0 +1,15 @@
+package test_24264;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        bw.write(Math.round(Math.pow(n, 2)) + "\n2");
+        bw.close();
+    }
+}
