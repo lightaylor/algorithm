f41961c95a40e8da57a2d94ab6e425f691f34091
lightaylor <115taegyeong@gmail.com>
2024-06-22 11:04:56 +0900

    feat: add 11650

--- baekjoon/test_11650/Main.java
+++ baekjoon/test_11650/Main.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Stream;
 
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
