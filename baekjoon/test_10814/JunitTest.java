062e7c0ed2b18cd9db1fa1a88f009e392135c769
lightaylor <115taegyeong@gmail.com>
2024-06-23 21:35:13 +0900

    feat: add 10814

---
+++ baekjoon/test_10814/JunitTest.java
@@ -0,0 +1,59 @@
+package test_10814;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static java.lang.System.*;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                3
+                21 Junkyu
+                21 Dohyun
+                20 Sunyoung
+                """;
+        String expectedOutput = """
+                20 Sunyoung
+                21 Junkyu
+                21 Dohyun
+                """;
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+                2
+                9 a
+                11 b
+                """;
+        String expectedOutput = """
+                9 a
+                11 b
+                """;
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    public String test(String input) throws IOException {
+        setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        setOut(new PrintStream(stdout));
+
+        Main.main(new String[0]);
+
+        setIn(in);
+        setOut(new PrintStream(out));
+
+        return stdout.toString();
+    }
+}
