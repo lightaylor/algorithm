4b0bd675ad279b43c893182d2346b015dd1b8c93
lightaylor <115taegyeong@gmail.com>
2024-07-15 15:14:58 +0900

    feat: add 3085

--- baekjoon/silver2/test_3085/Main.java
+++ baekjoon/silver2/test_3085/Main.java
@@ -3,24 +3,28 @@ package silver2.test_3085;
 import java.io.*;
 
 public class Main {
-    private static int check(char[][] arr) {
+    private static int check(char[][] arr, int x1, int y1, int x2, int y2) {
         int n = arr.length;
         int result = 1;
 
-        for (int i = 0; i < n; i++) {
+        // 변경 행/열만 check
+        for (int i = x1; i <= x2; i++) {
             int count = 1;
-            for (int j = 1; j < n; j++) {
-                if (arr[i][j] == arr[i][j-1]) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[i][j] == arr[i][j+1]) {
                     count++;
-                } else {
+                }
+                else {
                     count = 1;
                 }
                 result = Math.max(result, count);
             }
+        }
 
-            count = 1;
-            for (int j = 1; j < n; j++) {
-                if (arr[j][i] == arr[j-1][i]) {
+        for (int i = y1; i <= y2; i++) {
+            int count = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j][i] == arr[j+1][i]) {
                     count++;
                 } else {
                     count = 1;
@@ -28,6 +32,29 @@ public class Main {
                 result = Math.max(result, count);
             }
         }
+
+//        모든 경우 확인
+//        for (int i = 0; i < n; i++) {
+//            int count = 1;
+//            for (int j = 1; j < n; j++) {
+//                if (arr[i][j] == arr[i][j-1]) {
+//                    count++;
+//                } else {
+//                    count = 1;
+//                }
+//                result = Math.max(result, count);
+//            }
+//
+//            count = 1;
+//            for (int j = 1; j < n; j++) {
+//                if (arr[j][i] == arr[j-1][i]) {
+//                    count++;
+//                } else {
+//                    count = 1;
+//                }
+//                result = Math.max(result, count);
+//            }
+//        }
 
         return result;
     }
@@ -55,14 +82,14 @@ public class Main {
             for (int y = 0; y < n; y++) {
                 if (y + 1 < n) {
                     swap(arr, x, y, x, y + 1);
-                    temp = check(arr);
+                    temp = check(arr, x, y, x, y + 1);
                     count = Math.max(temp, count);
                     swap(arr, x, y + 1, x, y);
                 }
 
                 if (x + 1 < n) {
                     swap(arr, x, y, x + 1, y);
-                    temp = check(arr);
+                    temp = check(arr, x, y, x + 1, y);
                     count = Math.max(temp, count);
                     swap(arr, x + 1, y, x, y);
                 }
