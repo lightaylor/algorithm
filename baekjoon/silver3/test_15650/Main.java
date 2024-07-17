80cd5345b61704cb5f33c294bc2d252e474b483d
lightaylor <115taegyeong@gmail.com>
2024-07-17 21:45:01 +0900

    feat: add 15650

--- baekjoon/silver3/test_15650/Main.java
+++ baekjoon/silver3/test_15650/Main.java
@@ -12,11 +12,29 @@ public class Main {
 
         int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         answer = new int[input[1]];
-        go(0, 1, input[0], input[1]);
+//        go(0, 1, input[0], input[1]);
+        go(1, 0, input[0], input[1]);
     }
 
-    static void go (int idx, int start, int n, int m) {
-        if (idx == m) {
+//    static void go (int idx, int start, int n, int m) {
+//        if (idx == m) {
+//            for (int i = 0; i < m; i++) {
+//                System.out.print(answer[i]);
+//                if (i < m - 1) {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//            return;
+//        }
+//
+//        for (int i = start; i <= n; i++) {
+//            answer[idx] = i;
+//            go (idx + 1, i + 1, n, m);
+//        }
+//    }
+    static void go (int idx, int selected, int n, int m) {
+        if (selected == m) {
             for (int i = 0; i < m; i++) {
                 System.out.print(answer[i]);
                 if (i < m - 1) {
@@ -27,9 +45,11 @@ public class Main {
             return;
         }
 
-        for (int i = start; i <= n; i++) {
-            answer[idx] = i;
-            go (idx + 1, i + 1, n, m);
+        if (idx <= n) {
+            answer[selected] = idx;
+            go(idx + 1, selected + 1, n, m);
+            answer[selected] = 0;
+            go(idx + 1, selected, n, m);
         }
     }
 }
