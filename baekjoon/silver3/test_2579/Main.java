26b11ae29db8623e2630d61e171bea88e1ecd33e
lightaylor <115taegyeong@gmail.com>
2024-07-17 12:59:01 +0900

    feat: add 2579

--- baekjoon/silver3/test_2579/Main.java
+++ baekjoon/silver3/test_2579/Main.java
@@ -16,7 +16,7 @@ public class Main {
         }
 
         answer[1] = arr[1];
-        if (n  > 2) {
+        if (n  > 1) {
             answer[2] = arr[1] + arr[2];
         }
         for (int i = 3; i <= n; i++) {
