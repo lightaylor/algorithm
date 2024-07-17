f97db6f6186551cd0371dcc34f18663810951249
lightaylor <115taegyeong@gmail.com>
2024-07-17 22:17:40 +0900

    refactor: 15651 sout -> StringBuffer

--- baekjoon/silver3/test_15651/Main.java
+++ baekjoon/silver3/test_15651/Main.java
@@ -5,24 +5,27 @@ import java.util.stream.Stream;
 
 public class Main {
     private static int[] answer;
+    private static StringBuffer sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuffer();
 
         int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         answer = new int[input[1]];
         dfs(0, input[0], input[1]);
+        System.out.print(sb);
     }
 
-    static void dfs (int idx, int n, int m) {
+    static void dfs (int idx, int n, int m) throws IOException {
         if (idx == m) {
             for (int i = 0; i < m; i++) {
-                System.out.print(answer[i]);
+                sb.append(answer[i]);
                 if (i < m - 1) {
-                    System.out.print(" ");
+                    sb.append(" ");
                 }
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
 
