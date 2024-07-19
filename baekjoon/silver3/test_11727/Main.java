a437a8540eb4a4bfc2890c11a3bc4438c259c25f
lightaylor <115taegyeong@gmail.com>
2024-07-19 21:19:27 +0900

    fix: 11727

--- baekjoon/silver3/test_11727/Main.java
+++ baekjoon/silver3/test_11727/Main.java
@@ -2,7 +2,7 @@ package silver3.test_11727;
 
 import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,7 +13,11 @@ public class Main {
         dp[2] = 3;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10_007;
+            if (i % 2 == 0) {
+                dp[i] = (dp[i - 1] * 2 + 1) % 10_007;
+            } else {
+                dp[i] = (dp[i - 1] * 2 - 1) % 10_007;
+            }
         }
 
         bw.write(String.valueOf(dp[n]));
