fd8ac4051a4fba5cc2dbd8481e79cdb943372415
lightaylor <115taegyeong@gmail.com>
2024-08-09 14:10:00 +0900

    feat: add 10971

---
+++ baekjoon/silver2/test_10971/JunitTest.java
@@ -0,0 +1,40 @@
+package silver2.test_10971;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                4
+                0 10 15 20
+                5 0 9 10
+                6 13 0 12
+                8 8 9 0
+                """;
+        String expectedOutput = "35";
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    public String test(String input) throws IOException {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(stdout));
+
+        Main.main(new String[0]);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+        return stdout.toString();
+    }
+}
