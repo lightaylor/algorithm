c1f2f4b20f57376c02f8c2302bc993d5bae5a9ee
lightaylor <115taegyeong@gmail.com>
2024-08-09 18:38:10 +0900

    feat: add 1759

--- baekjoon/gold5/test_1759/Main.java
+++ baekjoon/gold5/test_1759/Main.java
@@ -1,55 +1,93 @@
 package gold5.test_1759;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
-    private static StringBuffer sb;
-    private static String[] answer;
-    private static String[] word;
-    private static String[] aeiou = {"a", "e", "i", "o", "u"};
+    private static BufferedWriter bw;
+    private static String[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuffer();
-        int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        word = br.readLine().split(" ");
-        answer = new String[input[0]];
-        Arrays.sort(word);
-        dfs(0, 0, input[0], input[1]);
-        System.out.print(sb);
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] n = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        arr = br.readLine().split(" ");
+        Arrays.sort(arr);
+        go(n[0], "", 0);
+        bw.close();
     }
 
-    static void dfs (int idx, int selected, int l, int c) {
-        if (idx == l) {
-            if (checkSpell()) {
-                StringBuilder temp = new StringBuilder();
-                for(String s : answer) {
-                    temp.append(s);
-                }
-                sb.append(temp).append("\n");
+    private static void go(int n, String password, int i) throws IOException {
+        if (password.length() == n) {
+            if (check(password)) {
+                bw.write(password);
+                bw.newLine();
             }
             return;
         }
-
-        for (int i = selected; i < c; i++) {
-            answer[idx] = word[i];
-            dfs(idx + 1, i + 1, l, c);
-        }
+        if (i >= arr.length) return;
+        go(n, password + arr[i], i + 1);
+        go(n, password, i + 1);
     }
 
-    static boolean checkSpell () {
+    private static boolean check(String password) {
+        String[] spellArr = {"a", "e", "i", "o", "u"};
         int count = 0;
 
-        for (String a : answer) {
-            for (String s : aeiou) {
-                count += a.equals(s) ? 1 : 0;
-            }
+        for (String spell : spellArr) {
+            if (password.contains(spell)) count += 1;
         }
 
-        return count > 0 && answer.length - count > 1;
+        return password.length() - count > 1 && count > 0;
     }
 }
+
+//public class Main {
+//    private static StringBuffer sb;
+//    private static String[] answer;
+//    private static String[] word;
+//    private static final String[] aeiou = {"a", "e", "i", "o", "u"};
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        sb = new StringBuffer();
+//        int[] input = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//        word = br.readLine().split(" ");
+//        answer = new String[input[0]];
+//        Arrays.sort(word);
+//        dfs(0, 0, input[0], input[1]);
+//        System.out.print(sb);
+//    }
+//
+//    static void dfs (int idx, int selected, int l, int c) {
+//        if (idx == l) {
+//            if (check()) {
+//                StringBuilder temp = new StringBuilder();
+//                for(String s : answer) {
+//                    temp.append(s);
+//                }
+//                sb.append(temp).append("\n");
+//            }
+//            return;
+//        }
+//
+//        for (int i = selected; i < c; i++) {
+//            answer[idx] = word[i];
+//            dfs(idx + 1, i + 1, l, c);
+//        }
+//    }
+//
+//    static boolean check () {
+//        int count = 0;
+//
+//        for (String a : answer) {
+//            for (String s : aeiou) {
+//                count += a.equals(s) ? 1 : 0;
+//            }
+//        }
+//
+//        return count > 0 && answer.length - count > 1;
+//    }
+//}
