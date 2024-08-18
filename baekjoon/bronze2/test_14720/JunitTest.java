b22a3e73581ab0b19588e3cbd4630cadcb47f5be
lightaylor <115taegyeong@gmail.com>
2024-08-18 09:55:33 +0700

    feat: add 14720

---
+++ baekjoon/bronze2/test_14720/JunitTest.java
@@ -0,0 +1,37 @@
+package bronze2.test_14720;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                7
+                0 1 2 0 1 2 0
+                """;
+        String expectedOutput = "7";
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    public String test(String input) throws IOException {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(stdout));
+
+        Main.main(new String[0]);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+        return stdout.toString();
+    }
+}
