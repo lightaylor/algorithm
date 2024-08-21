d9f50373e78ae45936b09def7ce14ab7fd90d8ad
lightaylor <115taegyeong@gmail.com>
2024-08-21 22:05:59 +0700

    feat: add 6749

---
+++ baekjoon/bronze4/test_6749/Main.java
@@ -0,0 +1,16 @@
+package bronze4.test_6749;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(2 * m - n));
+        bw.close();
+    }
+}
