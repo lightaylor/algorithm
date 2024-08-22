f06afbf3ae349e561c3a763b545e0fc9ffe7ffac
lightaylor <115taegyeong@gmail.com>
2024-08-22 13:07:45 +0700

    feat: add 11675

---
+++ baekjoon/silver5/test_11675/JunitTest.java
@@ -0,0 +1,37 @@
+package silver5.test_11675;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                5
+                58 89 205 20 198
+                """;
+        String expectedOutput = "22 55 187 12 66 ";
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    public String test(String input) throws IOException {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(stdout));
+
+        Main.main(new String[0]);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+        return stdout.toString();
+    }
+}
