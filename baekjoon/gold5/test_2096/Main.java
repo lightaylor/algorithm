86d64391369c2aeb3806b60dea89c79347d04705
lightaylor <115taegyeong@gmail.com>
2024-09-08 15:47:46 +0700

    refactor: 2920

--- baekjoon/gold5/test_2096/Main.java
+++ baekjoon/gold5/test_2096/Main.java
@@ -9,29 +9,30 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] board = new int[n+1][3];
+        int[][] board = new int[n + 1][3];
 
         for (int i = 1; i <= n; i++) {
             board[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        int[][] minDp = new int[n + 1][3];
         int[][] maxDp = new int[n + 1][3];
-        for(int i = 1; i <= n; i++) {
-            maxDp[i][0] += Math.max(maxDp[i - 1][0], maxDp[i - 1][1]) + board[i][0];
-            maxDp[i][1] += Math.max(Math.max(maxDp[i - 1][0], maxDp[i - 1][1]), maxDp[i - 1][2]) + board[i][1];
-            maxDp[i][2] += Math.max(maxDp[i - 1][1], maxDp[i - 1][2]) + board[i][2];
+        int[][] minDp = new int[n + 1][3];
 
-            minDp[i][0] += Math.min(minDp[i - 1][0], minDp[i - 1][1]) + board[i][0];
-            minDp[i][1] += Math.min(Math.min(minDp[i - 1][0], minDp[i - 1][1]), minDp[i - 1][2]) + board[i][1];
-            minDp[i][2] += Math.min(minDp[i - 1][1], minDp[i - 1][2]) + board[i][2];
+        for (int i = 1; i <= n; i++) {
+            maxDp[i][0] = board[i][0] + Math.max(maxDp[i - 1][0], maxDp[i - 1][1]);
+            maxDp[i][1] = board[i][1] + Math.max(maxDp[i - 1][0], Math.max(maxDp[i - 1][1], maxDp[i - 1][2]));
+            maxDp[i][2] = board[i][2] + Math.max(maxDp[i - 1][1], maxDp[i - 1][2]);
+
+            minDp[i][0] = board[i][0] + Math.min(minDp[i - 1][0], minDp[i - 1][1]);
+            minDp[i][1] = board[i][1] + Math.min(minDp[i - 1][0], Math.min(minDp[i - 1][1], minDp[i - 1][2]));
+            minDp[i][2] = board[i][2] + Math.min(minDp[i - 1][1], minDp[i - 1][2]);
         }
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < 3; i++) {
-            min = Math.min(min, minDp[n][i]);
+        int max = maxDp[n][0];
+        int min = minDp[n][0];
+        for (int i = 1; i < 3; i++) {
             max = Math.max(max, maxDp[n][i]);
+            min = Math.min(min, minDp[n][i]);
         }
 
         bw.write(max + " " + min);
