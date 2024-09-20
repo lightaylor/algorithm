f1a03782719bdc735f8672deeb2417588c6e0a3a
lightaylor <115taegyeong@gmail.com>
2024-09-20 11:14:54 +0900

    feat: 1707

---
+++ baekjoon/gold4/test_1707/JunitTest.java
@@ -0,0 +1,47 @@
+package gold4.test_1707;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                2
+                3 2
+                1 3
+                2 3
+                4 4
+                1 2
+                2 3
+                3 4
+                4 2
+                """;
+        String expectedOutput = """
+                YES
+                NO
+                """;
+
+        assertEquals(expectedOutput, test(input));
+    }
+
+    public String test(String input) throws IOException {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(stdout));
+
+        Main.main(new String[0]);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+        return stdout.toString();
+    }
+}
