fb1d29f73df66851dc65dda472469678693ab168
lightaylor <115taegyeong@gmail.com>
2024-09-28 13:09:13 +0900

    feat: 1676

---
+++ baekjoon/silver5/test_1676/Main.java
@@ -0,0 +1,18 @@
+package silver5.test_1676;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 5; i <= n; i *= 5) {
+            count += n / i;
+        }
+
+        System.out.print(count);
+    }
+}
