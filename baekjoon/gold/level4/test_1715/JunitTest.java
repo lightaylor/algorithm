f24e42a4aada761ca00574def1faefd3bb964bb1
lightaylor <115taegyeong@gmail.com>
2024-10-05 13:49:20 +0900

    feat: 1715 풀이 추가

---
+++ baekjoon/gold/level4/test_1715/JunitTest.java
@@ -0,0 +1,41 @@
+package gold.level4.test_1715;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                3
+                10
+                20
+                40
+                """;
+        String expectedOutput = "100";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+                4
+                10
+                20
+                40
+                30
+                """;
+        String expectedOutput = "190";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
