d077e1da09f44070011aa51bb1b65f4d214ef9de
lightaylor <115taegyeong@gmail.com>
2024-10-06 10:15:16 +0900

    feat: 1339 풀이 추가

---
+++ baekjoon/gold/level4/test_1339/JunitTest.java
@@ -0,0 +1,68 @@
+package gold.level4.test_1339;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                2
+                AAA
+                AAA
+                """;
+        String expectedOutput = "1998";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+                2
+                GCF
+                ACDEB
+                """;
+        String expectedOutput = "99437";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+                10
+                A
+                B
+                C
+                D
+                E
+                F
+                G
+                H
+                I
+                J
+                """;
+        String expectedOutput = "45";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = """
+                2
+                AB
+                BA
+                """;
+        String expectedOutput = "187";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
