13285aa1bad54fcd30eeb30a85c80664648e5ee1
lightaylor <115taegyeong@gmail.com>
2024-10-11 09:57:54 +0900

    feat: 3053 풀이 추가

---
+++ baekjoon/bronze/level3/test_3053/JunitTest.java
@@ -0,0 +1,48 @@
+package bronze.level3.test_3053;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "1";
+        String expectedOutput = """
+            3.141593
+            2.000000
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "21";
+        String expectedOutput = """
+            1385.442360
+            882.000000
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "42";
+        String expectedOutput = """
+            5541.769441
+            3528.000000
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level3/test_3053/Main.java
@@ -0,0 +1,18 @@
+package bronze.level3.test_3053;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        double euclideanArea = a * a * Math.PI;
+        double taxiArea = a * a * 2;
+
+        System.out.printf("%.6f\n", euclideanArea);
+        System.out.printf("%.6f\n", taxiArea);
+    }
+}
