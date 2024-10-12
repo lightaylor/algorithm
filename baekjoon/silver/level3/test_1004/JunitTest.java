8495209dea24409525b9d1b838226e224330fed8
lightaylor <115taegyeong@gmail.com>
2024-10-12 10:49:01 +0900

    feat: 1004 풀이 추가

---
+++ baekjoon/silver/level3/test_1004/JunitTest.java
@@ -0,0 +1,82 @@
+package silver.level3.test_1004;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            2
+            -5 1 12 1
+            7
+            1 1 8
+            -3 -1 1
+            2 2 2
+            5 5 1
+            -4 5 1
+            12 1 1
+            12 1 2
+            -5 1 5 1
+            1
+            0 0 2
+            """;
+        String expectedOutput = """
+            3
+            0
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            3
+            -5 1 5 1
+            3
+            0 0 2
+            -6 1 2
+            6 2 2
+            2 3 13 2
+            8
+            -3 -1 1
+            2 2 3
+            2 3 1
+            0 1 7
+            -4 5 1
+            12 1 1
+            12 1 2
+            12 1 3
+            102 16 19 -108
+            12
+            -107 175 135
+            -38 -115 42
+            140 23 70
+            148 -2 39
+            -198 -49 89
+            172 -151 39
+            -179 -52 43
+            148 42 150
+            176 0 10
+            153 68 120
+            -56 109 16
+            -187 -174 8
+            """;
+        String expectedOutput = """
+            2
+            5
+            3
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level3/test_1004/Main.java
@@ -0,0 +1,47 @@
+package silver.level3.test_1004;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int n  = Integer.parseInt(br.readLine());
+
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
+                int cx = Integer.parseInt(st.nextToken());
+                int cy = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+
+                boolean startInside = isInside(x1, y1, cx, cy, r);
+                boolean endInside = isInside(x2, y2, cx, cy, r);
+
+                if (startInside != endInside) {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+
+    private static boolean isInside(int x, int y, int cx, int cy, int r) {
+        int dx = x - cx;
+        int dy = y - cy;
+        return dx * dx + dy * dy < r * r;
+    }
+}
