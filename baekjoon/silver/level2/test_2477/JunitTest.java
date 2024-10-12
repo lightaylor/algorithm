42729d08f1e5f3a56bc5dff46c5ba6cbba6279fa
lightaylor <115taegyeong@gmail.com>
2024-10-12 11:57:04 +0900

    feat: 2477 풀이 추가

---
+++ baekjoon/silver/level2/test_2477/JunitTest.java
@@ -0,0 +1,46 @@
+package silver.level2.test_2477;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+                7
+                4 50
+                2 160
+                3 30
+                1 60
+                3 20
+                1 100
+                """;
+        String expectedOutput = "47600";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+   @Test
+    public void test_2() throws IOException {
+        String input = """
+                1
+                2 5
+                3 5
+                1 1
+                4 2
+                1 4
+                4 3
+                """;
+        String expectedOutput = "17";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level2/test_2477/Main.java
@@ -0,0 +1,38 @@
+package silver.level2.test_2477;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] directions = new int[6];
+        int[] lengths = new int[6];
+        int[] maxValue = new int[2];
+        int[] maxIndex = new int[2];
+
+        for (int i = 0; i < 6; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            directions[i] = Integer.parseInt(st.nextToken());
+            lengths[i] = Integer.parseInt(st.nextToken());
+
+            if (directions[i] < 3 && maxValue[0] < lengths[i]) {
+                maxValue[0] = lengths[i];
+                maxIndex[0] = i;
+            } else if (directions[i] > 2 && maxValue[1] < lengths[i]){
+                maxValue[1] = lengths[i];
+                maxIndex[1] = i;
+            }
+        }
+
+        int minValue = Math.abs(lengths[(maxIndex[0] + 5) % 6] - lengths[(maxIndex[0] + 1) % 6])
+                        * Math.abs(lengths[(maxIndex[1] + 5) % 6] - lengths[(maxIndex[1] + 1) % 6]);
+        int answer = (maxValue[0] * maxValue[1] - minValue) * N;
+
+        System.out.print(answer);
+    }
+}
