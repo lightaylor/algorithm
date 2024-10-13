16bb8159b631c59dab34e96e54517d9307903c23
lightaylor <115taegyeong@gmail.com>
2024-10-13 09:36:17 +0900

    feat: 3034 풀이 추가

---
+++ baekjoon/bronze/level3/test_3034/JunitTest.java
@@ -0,0 +1,51 @@
+package bronze.level3.test_3034;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5 3 4
+            3
+            4
+            5
+            6
+            7
+            """;
+        String expectedOutput = """
+            DA
+            DA
+            DA
+            NE
+            NE
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            2 12 17
+            21
+            20
+            """;
+        String expectedOutput = """
+            NE
+            DA
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+
+}
