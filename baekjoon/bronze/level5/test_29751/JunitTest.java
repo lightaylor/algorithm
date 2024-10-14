2a28f7c2cc115491d18f7a4bb99d54e4bde66b7d
lightaylor <115taegyeong@gmail.com>
2024-10-14 09:58:36 +0900

    feat: 29751 풀이 추가

---
+++ baekjoon/bronze/level5/test_29751/JunitTest.java
@@ -0,0 +1,30 @@
+package bronze.level5.test_29751;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "1 1";
+        String expectedOutput = "0.5";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "2 3";
+        String expectedOutput = "3.0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level5/test_29751/Main.java
@@ -0,0 +1,19 @@
+package bronze.level5.test_29751;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        double area = (double) (w * h) / 2;
+
+        System.out.printf("%.1f", area);
+    }
+}
