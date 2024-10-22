ed9ba81965fcb08909b9488129d80de04a7010d9
lightaylor <115taegyeong@gmail.com>
2024-10-22 09:57:14 +0900

    feat: 6502 풀이 추가

---
+++ baekjoon/bronze/level2/test_6502/Main.java
@@ -0,0 +1,34 @@
+package bronze.level2.test_6502;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int idx = 1;
+
+        while (!input.equals("0")) {
+            String[] arr = input.split(" ");
+
+            isValid(idx++, arr);
+
+            input = br.readLine();
+        }
+    }
+
+    private static void isValid(int idx, String[] arr) {
+        int r = Integer.parseInt(arr[0]);
+        int w = Integer.parseInt(arr[1]);
+        int l = Integer.parseInt(arr[2]);
+
+        System.out.print("Pizza " + idx);
+        if (r + r >= Math.sqrt(w * w + l * l)) {
+            System.out.println(" fits on the table.");
+        } else {
+            System.out.println(" does not fit on the table.");
+        }
+    }
+}
