12f981e81f90b9b7ba26bef373fb3d31fe601c4a
lightaylor <115taegyeong@gmail.com>
2024-10-23 10:47:35 +0900

    fix: 2156 풀이 추가

--- baekjoon/silver/level1/test_2156/Main.java
+++ baekjoon/silver/level1/test_2156/Main.java
@@ -17,9 +17,6 @@ public class Main {
 
         dp[1] = wine[1];
         if (n >= 2) dp[2] = wine[1] + wine[2];
-        if (n >= 3) {
-            dp[3] = Math.max(dp[2], Math.max(wine[1] + wine[3], wine[2] + wine[3]));
-        }
 
         for (int i = 3; i <= n; i++) {
             dp[i] = Math.max(dp[i - 1],
