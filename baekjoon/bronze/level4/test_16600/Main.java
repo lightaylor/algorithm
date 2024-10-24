7ebecf44cacab9544ac222414ba01368fe3a8b19
lightaylor <115taegyeong@gmail.com>
2024-10-24 11:00:04 +0900

    fix: 16600 풀이 추가

---
+++ baekjoon/bronze/level4/test_16600/Main.java
@@ -0,0 +1,14 @@
+package bronze.level4.test_16600;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+
+        System.out.print(Math.sqrt(a) * 4);
+    }
+}
