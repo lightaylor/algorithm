b0568374878b4616c84bcd8623e3d48f123da3e6
lightaylor <115taegyeong@gmail.com>
2024-10-25 10:01:30 +0900

    fix: 9366 풀이 추가

---
+++ baekjoon/bronze/level3/test_9366/JunitTest.java
@@ -0,0 +1,27 @@
+package bronze.level3.test_9366;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            2
+            3 3 4
+            6 4 2
+            """;
+        String expectedOutput = """
+            Case #1: isosceles
+            Case #2: invalid!
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
---
+++ baekjoon/bronze/level3/test_9366/Main.java
@@ -0,0 +1,30 @@
+package bronze.level3.test_9366;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import static java.util.Arrays.stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            int[] arr = stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(arr);
+
+            if (arr[0] + arr[1] <= arr[2]) {
+                System.out.println("Case #" + i + ": invalid!");
+            } else if (arr[0] == arr[1] && arr[1] == arr[2]) {
+                System.out.println("Case #" + i + ": equilateral");
+            } else if (arr[0] == arr[1] || arr[1] == arr[2] || arr[2] == arr[0]) {
+                System.out.println("Case #" + i + ": isosceles");
+            } else {
+                System.out.println("Case #" + i + ": scalene");
+            }
+        }
+    }
+}
