c8fb9b51a07a06d643bd762ee1d3169bd3035300
lightaylor <115taegyeong@gmail.com>
2024-10-27 10:36:54 +0900

    feat: 11721 풀이 추가

---
+++ baekjoon/bronze/level3/test_11721/JunitTest.java
@@ -0,0 +1,38 @@
+package bronze.level3.test_11721;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "BaekjoonOnlineJudge";
+        String expectedOutput = """
+            BaekjoonOn
+            lineJudge
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "OneTwoThreeFourFiveSixSevenEightNineTen";
+        String expectedOutput = """
+            OneTwoThre
+            eFourFiveS
+            ixSevenEig
+            htNineTen
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
