f42cc9f2bd7c0ec7828b4cd6059c640a0ab35ed3
lightaylor <115taegyeong@gmail.com>
2024-10-29 16:32:11 +0900

    feat: 2864 풀이 추가

---
+++ baekjoon/bronze/level2/test_2864/Main.java
@@ -0,0 +1,24 @@
+package bronze.level2.test_2864;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        int min = Integer.parseInt(A.replaceAll("6", "5")) +
+            Integer.parseInt(B.replaceAll("6", "5"));
+
+        int max = Integer.parseInt(A.replaceAll("5", "6")) +
+            Integer.parseInt(B.replaceAll("5", "6"));
+
+        System.out.print(min + " " + max);
+    }
+}
