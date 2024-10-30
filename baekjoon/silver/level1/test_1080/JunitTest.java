95f1a4d408fae19178d27ee404d7f766738d66fd
lightaylor <115taegyeong@gmail.com>
2024-10-31 01:13:01 +0900

    feat: 1080 풀이 추가

---
+++ baekjoon/silver/level1/test_1080/JunitTest.java
@@ -0,0 +1,104 @@
+package silver.level1.test_1080;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3 4
+            0000
+            0010
+            0000
+            1001
+            1011
+            1001
+            """;
+        String expectedOutput = "2";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            3 3
+            111
+            111
+            111
+            000
+            000
+            000
+            """;
+        String expectedOutput = "1";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            1 1
+            1
+            0
+            """;
+        String expectedOutput = "-1";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = """
+            18 3
+            001
+            100
+            100
+            000
+            011
+            010
+            100
+            100
+            010
+            010
+            010
+            110
+            101
+            101
+            000
+            110
+            000
+            110
+            001
+            100
+            011
+            000
+            100
+            010
+            011
+            100
+            101
+            101
+            010
+            001
+            010
+            010
+            111
+            110
+            111
+            001
+            """;
+        String expectedOutput = "7";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level1/test_1080/Main.java
@@ -0,0 +1,64 @@
+package silver.level1.test_1080;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N;
+    private static int M;
+    private static int[][] A;
+    private static int[][] B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new int[N][M];
+        B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                A[i][j] = line.charAt(j) - '0';
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                B[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i <= N - 3; i++) {
+            for (int j = 0; j <= M - 3; j++) {
+                if (A[i][j] == B[i][j]) continue;
+                reverse(i, j);
+                count++;
+            }
+        }
+
+        System.out.print(areEquals() ? count : -1);
+    }
+
+    private static void reverse(int iStart, int jStart) {
+        for (int i = iStart; i < iStart + 3; i++) {
+            for (int j = jStart; j < jStart + 3; j++) {
+                A[i][j] = A[i][j] == 1 ? 0 : 1;
+            }
+        }
+    }
+
+    private static boolean areEquals() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (A[i][j] != B[i][j]) return false;
+            }
+        }
+        return true;
+    }
+}
