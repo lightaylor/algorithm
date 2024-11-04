1e7c2e9a6268515ad03801173ef2e2a50dfb6be6
lightaylor <115taegyeong@gmail.com>
2024-11-04 13:24:22 +0900

    feat: 16937 풀이 추가

---
+++ baekjoon/silver/level3/test_16937/JunitTest.java
@@ -0,0 +1,57 @@
+package silver.level3.test_16937;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            2 2
+            2
+            1 2
+            2 1
+            """;
+        String expectedOutput = "4";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            10 9
+            4
+            2 3
+            1 1
+            5 10
+            9 11
+            """;
+        String expectedOutput = "56";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            10 10
+            3
+            6 6
+            7 7
+            20 5
+            """;
+        String expectedOutput = "0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
