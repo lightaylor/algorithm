8921afe8b4b837447a6852120458d2193e70daa0
lightaylor <115taegyeong@gmail.com>
2024-11-06 18:46:19 +0900

    feat: 16479 풀이 추가

---
+++ baekjoon/bronze/level3/test_16479/Main.java
@@ -0,0 +1,22 @@
+package bronze.level3.test_16479;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        double diff = (double) (a - b) / 2;
+
+        double answer = Math.pow(k, 2) - Math.pow(diff, 2);
+        System.out.print(answer);
+    }
+}
