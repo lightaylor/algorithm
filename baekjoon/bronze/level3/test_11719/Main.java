d6e22760dd6e0d1e326c6578bb45284170221fc8
lightaylor <115taegyeong@gmail.com>
2024-11-08 09:25:03 +0900

    feat: 11719 풀이 추가

---
+++ baekjoon/bronze/level3/test_11719/Main.java
@@ -0,0 +1,17 @@
+package bronze.level3.test_11719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        while(input != null) {
+            System.out.println(input);
+            input = br.readLine();
+        }
+    }
+}
