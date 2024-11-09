7b99f2fd0206e6d49254d22ee635b9e801bbd993
lightaylor <115taegyeong@gmail.com>
2024-11-10 08:49:59 +0900

    feat: 20353 풀이 추가

---
+++ baekjoon/bronze/level4/test_20353/Main.java
@@ -0,0 +1,12 @@
+package bronze.level4.test_20353;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double length = Math.sqrt(a);
+        System.out.print(length * 4);
+    }
+}
