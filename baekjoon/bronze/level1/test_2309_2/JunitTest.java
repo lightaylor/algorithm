26c54e1b622ff282511104417c27ccd9ffa6eb72
lightaylor <115taegyeong@gmail.com>
2024-11-10 21:33:58 +0900

    feat: 2309 풀이 추가

--- baekjoon/bronze/level1/test_2309_2/JunitTest.java
+++ baekjoon/bronze/level1/test_2309_2/JunitTest.java
@@ -12,25 +12,25 @@ public class JunitTest {
     @Test
     public void test_1() throws IOException {
         String input = """
-                20
-                7
-                23
-                19
-                10
-                15
-                25
-                8
-                13
-                """;
+            20
+            7
+            23
+            19
+            10
+            15
+            25
+            8
+            13
+            """;
         String expectedOutput = """
-                7
-                8
-                10
-                13
-                19
-                20
-                23
-                """;
+            7
+            8
+            10
+            13
+            19
+            20
+            23
+            """;
 
         String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
         assertEquals(expectedOutput, actualOutput);
@@ -39,26 +39,26 @@ public class JunitTest {
     @Test
     public void test_2() throws IOException {
         String input = """
-                40
-                40
-                10
-                10
-                10
-                10
-                10
-                10
-                10
-                """;
+            40
+            40
+            10
+            10
+            10
+            10
+            10
+            10
+            10
+            """;
         String expectedOutput = """
-                10
-                10
-                10
-                10
-                10
-                10
-                40
-                """;
-        
+            10
+            10
+            10
+            10
+            10
+            10
+            40
+            """;
+
         String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
         assertEquals(expectedOutput, actualOutput);
     }
