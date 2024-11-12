d765f4c7b18c8190c28446f5735e6b1dc60334d3
lightaylor <115taegyeong@gmail.com>
2024-11-12 09:03:03 +0900

    feat: 16483 풀이 추가

---
+++ baekjoon/bronze/level3/test_16483/JunitTest.java
@@ -0,0 +1,20 @@
+package bronze.level3.test_16483;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "17";
+        String expectedOutput = "72";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
---
+++ baekjoon/bronze/level3/test_16483/Main.java
@@ -0,0 +1,12 @@
+package bronze.level3.test_16483;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float T = sc.nextInt();
+        float answer = Math.round(Math.pow(T, 2)/4);
+        System.out.print((int) answer);
+    }
+}
