02d5737ec10cef1e5006e7e52e8f47295effc139
lightaylor <115taegyeong@gmail.com>
2024-11-16 10:48:09 +0900

    feat: v3 1748 풀이 추가

---
+++ baekjoon/silver/level4/test_1748/v3/Main.java
@@ -0,0 +1,20 @@
+package silver.level4.test_1748.v3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int answer = 0;
+
+        int count = 1;
+        for (int i = 1; i <= N; i *= 10) {
+            int end = Math.min(i * 10 - 1, N);
+            answer += (end - i + 1) * count++;
+        }
+
+        System.out.print(answer);
+    }
+}
