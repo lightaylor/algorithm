523cc4269eb979f02cf73ec566a3ed9c4235e06a
lightaylor <115taegyeong@gmail.com>
2024-11-16 12:15:55 +0900

    feat: v3 15649 풀이 추가

---
+++ baekjoon/silver/level3/test_15649/v3/Main.java
@@ -0,0 +1,34 @@
+package silver.level3.test_15649.v3;
+
+import java.util.Scanner;
+
+public class Main {
+    private static boolean[] check;
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        check = new boolean[N + 1];
+        dfs(0, new StringBuilder());
+    }
+
+    private static void dfs(int idx, StringBuilder sb) {
+        if (idx == M) {
+            System.out.println(sb.toString().trim());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                sb.append(i).append(" ");
+                dfs(idx + 1, sb);
+                sb.delete(sb.length() - 2, sb.length());
+                check[i] = false;
+            }
+        }
+    }
+}
