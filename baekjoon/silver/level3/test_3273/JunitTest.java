e3e4d46f1acf7658de3e2436f96b03213a0389fe
lightaylor <115taegyeong@gmail.com>
2024-11-18 14:00:42 +0900

    feat: 3273 풀이 추가

---
+++ baekjoon/silver/level3/test_3273/JunitTest.java
@@ -0,0 +1,24 @@
+package silver.level3.test_3273;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            9
+            5 12 7 10 9 1 2 3 11
+            13
+            """;
+        String expectedOutput = "3";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level3/test_3273/Main.java
@@ -0,0 +1,36 @@
+package silver.level3.test_3273;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+        int answer = 0;
+        Arrays.sort(arr);
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == x) {
+                answer++;
+                left++;
+                right--;
+            } else if (sum < x) {
+                left++;
+            } else{
+                right--;
+            }
+        }
+        System.out.print(answer);
+    }
+}
