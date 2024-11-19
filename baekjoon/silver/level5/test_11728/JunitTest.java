76c3c7d03d905efb4445b1cbc3ab9d9ebb7cb13d
lightaylor <115taegyeong@gmail.com>
2024-11-19 12:34:16 +0900

    feat: 11728 풀이 추가

---
+++ baekjoon/silver/level5/test_11728/JunitTest.java
@@ -0,0 +1,48 @@
+package silver.level5.test_11728;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            2 2
+            3 5
+            2 9
+            """;
+        String expectedOutput = "2 3 5 9";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            2 1
+            4 7
+            1
+            """;
+        String expectedOutput = "1 4 7";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            4 3
+            2 3 5 9
+            1 4 7
+            """;
+        String expectedOutput = "1 2 3 4 5 7 9";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level5/test_11728/Main.java
@@ -0,0 +1,47 @@
+package silver.level5.test_11728;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int[] AArr = new int[A];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < A; i++) {
+            AArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] BArr = new int[B];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < B; i++) {
+            BArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < A && j < B) {
+            if (AArr[i] < BArr[j]) {
+                sb.append(AArr[i++]).append(" ");
+            } else {
+                sb.append(BArr[j++]).append(" ");
+            }
+        }
+
+        while(i < A) {
+            sb.append(AArr[i++]).append(" ");
+        }
+
+        while(j < B) {
+            sb.append(BArr[j++]).append(" ");
+        }
+
+        System.out.print(sb.toString().trim());
+    }
+}
