7f3b3be4e955eb169a3629595afdb94e221c04cc
lightaylor <115taegyeong@gmail.com>
2024-11-21 11:42:28 +0900

    feat: 1940 풀이 추가

---
+++ baekjoon/silver/level4/test_1940/JunitTest.java
@@ -0,0 +1,25 @@
+package silver.level4.test_1940;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            6
+            9
+            2 7 4 1 5 3
+            """;
+        String expectedOutput = "2";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level4/test_1940/Main.java
@@ -0,0 +1,36 @@
+package silver.level4.test_1940;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] num = new int[N];
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextInt();
+        }
+        Arrays.sort(num);
+
+        int left = 0;
+        int right = N - 1;
+        int answer = 0;
+        while (left < right) {
+            if (num[left] + num[right] == M) {
+                answer++;
+                left++;
+                right--;
+            }
+
+            if (num[left] + num[right] > M) {
+                right--;
+            } else if (num[left] + num[right] < M) {
+                left++;
+            }
+        }
+        System.out.print(answer);
+    }
+}
