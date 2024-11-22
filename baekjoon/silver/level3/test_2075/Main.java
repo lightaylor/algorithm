866705a63fee1ad316d18bc95cd6ab8ac4174ff5
lightaylor <115taegyeong@gmail.com>
2024-11-22 10:36:57 +0900

    feat: 2075 풀이 추가

---
+++ baekjoon/silver/level3/test_2075/Main.java
@@ -0,0 +1,24 @@
+package silver.level3.test_2075;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                pq.add(sc.nextInt());
+                if (pq.size() > N) {
+                    pq.poll();
+                }
+            }
+        }
+
+        System.out.print(pq.peek());
+    }
+}
