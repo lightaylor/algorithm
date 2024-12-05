ae07f26821a983c16744a95348924b2806081d15
lightaylor <115taegyeong@gmail.com>
2024-12-05 20:32:27 +0900

    feat: v3 1759 풀이 추가

---
+++ baekjoon/gold/level5/test_1759/v3/JunitTest.java
@@ -0,0 +1,50 @@
+package gold.level5.test_1759.v3;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            4 6
+            a t c i s w
+            """;
+        String expectedOutput = """
+            acis
+            acit
+            aciw
+            acst
+            acsw
+            actw
+            aist
+            aisw
+            aitw
+            astw
+            cist
+            cisw
+            citw
+            istw
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            3 5
+            a e i o u
+            """;
+
+        String expectedOutput = "";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
