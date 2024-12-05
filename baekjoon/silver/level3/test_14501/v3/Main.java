400445299bee087d1bbc5991a9013e1666395baa
lightaylor <115taegyeong@gmail.com>
2024-12-05 21:39:54 +0900

    feat: v3 14501 풀이 추가

---
+++ baekjoon/silver/level3/test_14501/v3/Main.java
@@ -0,0 +1,28 @@
+package silver.level3.test_14501.v3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        int[] dp = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            int day = arr[i][0];
+            int cost = arr[i][1];
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+
+            if (i + day <= N) {
+                dp[i + day] = Math.max(dp[i + day], dp[i] + cost);
+            }
+        }
+        System.out.print(dp[N]);
+    }
+}
