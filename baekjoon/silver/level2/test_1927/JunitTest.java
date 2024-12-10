09a1bce128188cf1352a6998751b746a626698eb
lightaylor <115taegyeong@gmail.com>
2024-12-10 10:55:42 +0900

    feat: 1927 풀이 추가

---
+++ baekjoon/silver/level2/test_1927/JunitTest.java
@@ -0,0 +1,76 @@
+package silver.level2.test_1927;
+
+import common.TestUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static common.ThrowingRunnable.runUnchecked;
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            9
+            0
+            12345678
+            1
+            2
+            0
+            0
+            0
+            0
+            32
+            """;
+        String expectedOutput = """
+            0
+            1
+            2
+            12345678
+            0
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            17
+            1
+            10
+            2
+            30
+            40
+            300
+            400
+            25
+            0
+            0
+            0
+            0
+            0
+            0
+            0
+            0
+            0
+            0
+            """;
+        String expectedOutput = """
+            1
+            2
+            10
+            25
+            30
+            40
+            300
+            400
+            0
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
---
+++ baekjoon/silver/level2/test_1927/Main.java
@@ -0,0 +1,23 @@
+package silver.level2.test_1927;
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            if (x == 0) {
+                Integer first = queue.poll();
+                System.out.println(first != null ? first : 0);
+            } else {
+                queue.add(x);
+            }
+        }
+        sc.close();
+    }
+}
