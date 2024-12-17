33b7d3477adf9754e7be9a9c253a4225b2b05166
lightaylor <115taegyeong@gmail.com>
2024-12-17 21:04:45 +0900

    feat: 2667 풀이 추가

---
+++ baekjoon/silver/level1/test_2667/v3/JunitTest.java
@@ -0,0 +1,34 @@
+package silver.level1.test_2667.v3;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            7
+            0110100
+            0110101
+            1110101
+            0000111
+            0100000
+            0111110
+            0111000
+            """;
+        String expectedOutput = """
+            3
+            7
+            8
+            9
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
