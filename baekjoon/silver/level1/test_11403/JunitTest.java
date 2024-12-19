f47417e8702ef0a3454a9e783b1bf87c0251eecc
lightaylor <115taegyeong@gmail.com>
2024-12-19 20:21:46 +0900

    feat: 11403 풀이 추가

---
+++ baekjoon/silver/level1/test_11403/JunitTest.java
@@ -0,0 +1,55 @@
+package silver.level1.test_11403;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            0 1 0
+            0 0 1
+            1 0 0
+            """;
+        String expectedOutput = """
+            1 1 1
+            1 1 1
+            1 1 1
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            7
+            0 0 0 1 0 0 0
+            0 0 0 0 0 0 1
+            0 0 0 0 0 0 0
+            0 0 0 0 1 1 0
+            1 0 0 0 0 0 0
+            0 0 0 0 0 0 1
+            0 0 1 0 0 0 0
+            """;
+        String expectedOutput = """
+            1 0 1 1 1 1 1
+            0 0 1 0 0 0 1
+            0 0 0 0 0 0 0
+            1 0 1 1 1 1 1
+            1 0 1 1 1 1 1
+            0 0 1 0 0 0 1
+            0 0 1 0 0 0 0
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
---
+++ baekjoon/silver/level1/test_11403/Main.java
@@ -0,0 +1,35 @@
+package silver.level1.test_11403;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] graph = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < N; j++) {
+                sb.append(graph[i][j]).append(" ");
+            }
+            System.out.println(sb.toString().trim());
+        }
+    }
+}
