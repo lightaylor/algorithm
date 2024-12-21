ae1e3e04556596603e868b282b8de7e32913ea42
lightaylor <115taegyeong@gmail.com>
2024-12-21 18:41:36 +0900

    feat: 7569 풀이 추가

---
+++ baekjoon/gold/level5/test_7569/JunitTest.java
@@ -0,0 +1,59 @@
+package gold.level5.test_7569;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5 3 1
+            0 -1 0 0 0
+            -1 -1 0 1 1
+            0 0 0 1 1
+            """;
+        String expectedOutput = "-1";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            5 3 2
+            0 0 0 0 0
+            0 0 0 0 0
+            0 0 0 0 0
+            0 0 0 0 0
+            0 0 1 0 0
+            0 0 0 0 0
+            """;
+        String expectedOutput = "4";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            4 3 2
+            1 1 1 1
+            1 1 1 1
+            1 1 1 1
+            1 1 1 1
+            -1 -1 -1 -1
+            1 1 1 -1
+            """;
+        String expectedOutput = "0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
