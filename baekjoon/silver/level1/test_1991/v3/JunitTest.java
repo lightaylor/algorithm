8e6711fee7c6c2a761f147467b4cd4d89920e977
lightaylor <115taegyeong@gmail.com>
2024-12-23 23:00:00 +0900

    feat: v3 1991 풀이 추가

---
+++ baekjoon/silver/level1/test_1991/v3/JunitTest.java
@@ -0,0 +1,35 @@
+package silver.level1.test_1991.v3;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            7
+            A B C
+            B D .
+            C E F
+            E . .
+            F . G
+            D . .
+            G . .
+            """;
+        String expectedOutput = """
+            ABDCEFG
+            DBAECFG
+            DBEGFCA
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
