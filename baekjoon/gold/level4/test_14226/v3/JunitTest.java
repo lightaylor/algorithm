00098d5d23863d6e3d77385e8870a177b3488ca8
lightaylor <115taegyeong@gmail.com>
2024-12-23 20:42:00 +0900

    feat: v3 14226 풀이 추가

---
+++ baekjoon/gold/level4/test_14226/v3/JunitTest.java
@@ -0,0 +1,47 @@
+package gold.level4.test_14226.v3;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "2";
+        String expectedOutput = "2";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "4";
+        String expectedOutput = "4";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "6";
+        String expectedOutput = "5";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = "18";
+        String expectedOutput = "8";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
---
+++ baekjoon/gold/level4/test_14226/v3/Main.java
@@ -0,0 +1,58 @@
+package gold.level4.test_14226.v3;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int S = sc.nextInt();
+
+        bfs(S);
+    }
+
+    private static void bfs(int S) {
+        Queue<Status> queue = new LinkedList<>();
+        queue.add(new Status(1, 0, 0));
+        boolean[][] visited = new boolean[2001][2001];
+        visited[1][0] = true;
+
+        while (!queue.isEmpty()) {
+            Status status = queue.poll();
+            int screen = status.screen;
+            int clipboard = status.clipboard;
+            int sec = status.sec;
+
+            if (screen == S) {
+                System.out.print(sec);
+                return;
+            }
+
+            if (screen - 1 >= 0 && !visited[screen - 1][clipboard]) {
+                visited[screen - 1][clipboard] = true;
+                queue.add(new Status(screen - 1, clipboard, sec + 1));
+            }
+            if (screen > 0 && !visited[screen][screen]) {
+                visited[screen][screen] = true;
+                queue.add(new Status(screen, screen, sec + 1));
+            }
+            if (screen + clipboard <= 1000 && !visited[screen + clipboard][clipboard]) {
+                visited[screen + clipboard][clipboard] = true;
+                queue.add(new Status(screen + clipboard, clipboard, sec + 1));
+            }
+        }
+    }
+
+    static class Status {
+        int screen;
+        int clipboard;
+        int sec;
+
+        public Status(int screen, int clipboard, int sec) {
+            this.screen = screen;
+            this.clipboard = clipboard;
+            this.sec = sec;
+        }
+    }
+}
