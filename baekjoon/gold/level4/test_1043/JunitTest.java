506251d3b2d4645ea7dde29a0e6108645f80bcd0
lightaylor <115taegyeong@gmail.com>
2024-12-26 16:08:50 +0900

    feat: 1043 풀이 추가

---
+++ baekjoon/gold/level4/test_1043/JunitTest.java
@@ -0,0 +1,125 @@
+package gold.level4.test_1043;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            4 3
+            0
+            2 1 2
+            1 3
+            3 2 3 4
+            """;
+        String expectedOutput = "3";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            4 1
+            1 1
+            4 1 2 3 4
+            """;
+        String expectedOutput = "0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            4 1
+            0
+            4 1 2 3 4
+            """;
+        String expectedOutput = "1";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = """
+            4 5
+            1 1
+            1 1
+            1 2
+            1 3
+            1 4
+            2 4 1
+            """;
+        String expectedOutput = "2";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_5() throws IOException {
+        String input = """
+            10 9
+            4 1 2 3 4
+            2 1 5
+            2 2 6
+            1 7
+            1 8
+            2 7 8
+            1 9
+            1 10
+            2 3 10
+            1 4
+            """;
+        String expectedOutput = "4";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_6() throws IOException {
+        String input = """
+            8 5
+            3 1 2 7
+            2 3 4
+            1 5
+            2 5 6
+            2 6 8
+            1 8
+            """;
+        String expectedOutput = "5";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_7() throws IOException {
+        String input = """
+            3 4
+            1 3
+            1 1
+            1 2
+            2 1 2
+            3 1 2 3
+            """;
+        String expectedOutput = "0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
