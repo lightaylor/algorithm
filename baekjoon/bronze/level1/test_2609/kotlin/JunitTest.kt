87f4b61273d33d30a0f0bf655e9c69e0ba82d0be
lightaylor <115taegyeong@gmail.com>
2024-12-30 23:10:47 +0900

    feat: 2609 풀이 추가

---
+++ baekjoon/bronze/level1/test_2609/kotlin/JunitTest.kt
@@ -0,0 +1,21 @@
+package bronze.level1.test_2609.kotlin;
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = "24 18"
+        val expectedOutput = """
+            6
+            72
+        """.trimIndent()
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
---
+++ baekjoon/bronze/level1/test_2609/kotlin/main.kt
@@ -0,0 +1,16 @@
+package bronze.level1.test_2609.kotlin
+
+fun main() {
+    val n = readln().split(" ").map { it.toInt() }
+
+    println(gcd(n[0], n[1]))
+    print(lcm(n[0], n[1]))
+}
+
+fun lcm(a: Int, b: Int): Int {
+    return a * b / gcd(a, b)
+}
+
+fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) a else gcd(b, a % b)
+}
