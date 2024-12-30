844064a409447468830e24c6d42e324eac9244df
lightaylor <115taegyeong@gmail.com>
2024-12-30 23:30:57 +0900

    feat: 2748 풀이 추가

--- baekjoon/bronze/level1/test_2609/java/JunitTest.java
+++ baekjoon/bronze/level1/test_2609/java/JunitTest.java
@@ -1,21 +1,22 @@
 package bronze.level1.test_2609.java;
 
-import util.TestUtil;
 import org.junit.Test;
+import util.TestUtil;
 
 import java.io.IOException;
 
-import static util.ThrowingRunnable.runUnchecked;
 import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
 
 public class JunitTest {
+
     @Test
     public void test_1() throws IOException {
         String input = "24 18";
         String expectedOutput = """
-                6
-                72
-                """;
+            6
+            72
+            """;
         String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
         assertEquals(expectedOutput, actualOutput);
     }
---
+++ baekjoon/bronze/level1/test_2748/kotlin/JunitTest.kt
@@ -0,0 +1,18 @@
+package bronze.level1.test_2748.kotlin;
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = "10"
+        val expectedOutput = "55"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
