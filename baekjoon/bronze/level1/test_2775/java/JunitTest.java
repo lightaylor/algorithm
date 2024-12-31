ee398f5446690b0b71fa1eac3f5e2d247a124622
lightaylor <115taegyeong@gmail.com>
2024-12-31 18:07:14 +0900

    refactor: 파일 이름 변경

--- baekjoon/bronze/level1/test_2775/java/JunitTest.java
+++ baekjoon/bronze/level1/test_2775/java/JunitTest.java
@@ -1,18 +1,29 @@
 package bronze.level1.test_2775.java;
 
-import util.TestUtil;
 import org.junit.Test;
+import util.TestUtil;
 
 import java.io.IOException;
 
-import static util.ThrowingRunnable.runUnchecked;
 import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
 
 public class JunitTest {
+
     @Test
     public void test_1() throws IOException {
-        String input = "2\n1\n3\n2\n3";
-        String expectedOutput = "6\n10\n";
+        String input = """
+            2
+            1
+            3
+            2
+            3
+            """;
+        String expectedOutput = """
+            6
+            10
+            """;
+
         String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
         assertEquals(expectedOutput, actualOutput);
     }
