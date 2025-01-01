89e8dc9c0ee027f9629cb83db062a0d8ba28e64c
lightaylor <115taegyeong@gmail.com>
2025-01-01 21:41:33 +0900

    feat: 4796 풀이 추가

---
+++ baekjoon/bronze/level1/test_4796/kotlin/Main.kt
@@ -0,0 +1,19 @@
+package bronze.level1.test_4796.kotlin
+
+fun main() {
+
+    var index = 1
+    while (true) {
+        val input = readln().split(" ").map { it.toInt() }
+        val l = input[0]
+        val p = input[1]
+        val v = input[2]
+
+        if (l == 0 && p == 0 && v == 0) {
+            break
+        }
+
+        val answer = v / p * l + (v % p).coerceAtMost(l)
+        println("Case ${index++}: $answer")
+    }
+}
