4d69916b19fd19694b891cd1d12443fb56c8c41e
lightaylor <115taegyeong@gmail.com>
2025-01-06 23:44:43 +0900

    feat: 10798 풀이 추가

--- baekjoon/bronze/level1/test_10798/java/JunitTest.java
+++ baekjoon/bronze/level1/test_10798/java/JunitTest.java
@@ -1,23 +1,24 @@
-package bronze.level1.test_10798;
+package bronze.level1.test_10798.java;
 
-import util.TestUtil;
 import org.junit.Test;
+import util.TestUtil;
 
 import java.io.IOException;
 
-import static util.ThrowingRunnable.runUnchecked;
 import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
 
 public class JunitTest {
+    
     @Test
     public void test_1() throws IOException {
         String input = """
-                ABCDE
-                abcde
-                01234
-                FGHIJ
-                fghij
-                """;
+            ABCDE
+            abcde
+            01234
+            FGHIJ
+            fghij
+            """;
         String expectedOutput = "Aa0FfBb1GgCc2HhDd3IiEe4Jj";
         String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
         assertEquals(expectedOutput, actualOutput);
@@ -26,16 +27,15 @@ public class JunitTest {
     @Test
     public void test_2() throws IOException {
         String input = """
-                AABCDD
-                afzz
-                09121
-                a8EWg6
-                P5h3kx
-                """;
+            AABCDD
+            afzz
+            09121
+            a8EWg6
+            P5h3kx
+            """;
         String expectedOutput = "Aa0aPAf985Bz1EhCz2W3D1gkD6x";
         String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
         assertEquals(expectedOutput, actualOutput);
     }
-
 
 }
---
+++ baekjoon/bronze/level1/test_10798/kotlin/JunitTest.kt
@@ -0,0 +1,39 @@
+package bronze.level1.test_10798.kotlin;
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = """
+            ABCDE
+            abcde
+            01234
+            FGHIJ
+            fghij
+        """.trimIndent()
+        val expectedOutput = "Aa0FfBb1GgCc2HhDd3IiEe4Jj"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test2() {
+        val input = """
+            AABCDD
+            afzz
+            09121
+            a8EWg6
+            P5h3kx
+        """.trimIndent()
+        val expectedOutput = "Aa0aPAf985Bz1EhCz2W3D1gkD6x"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
