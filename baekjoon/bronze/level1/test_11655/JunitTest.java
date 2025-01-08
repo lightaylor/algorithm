2171565a25b3195ead01ca2e86fccff6e2fa2f38
lightaylor <115taegyeong@gmail.com>
2025-01-08 21:43:56 +0900

    feat: 11655 풀이 추가

---
+++ baekjoon/bronze/level1/test_11655/JunitTest.java
@@ -0,0 +1,30 @@
+package bronze.level1.test_11655;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "Baekjoon Online Judge";
+        String expectedOutput = "Onrxwbba Bayvar Whqtr";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "One is 1";
+        String expectedOutput = "Bar vf 1";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
