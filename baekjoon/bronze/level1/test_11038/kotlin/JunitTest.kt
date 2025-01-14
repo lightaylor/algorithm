a3c44bb98010320705029657db0430e7ac6a9a42
lightaylor <115taegyeong@gmail.com>
2025-01-14 21:41:46 +0900

    feat: 11038 풀이 추가

---
+++ baekjoon/bronze/level1/test_11038/kotlin/JunitTest.kt
@@ -0,0 +1,60 @@
+package bronze.level1.test_11038.kotlin;
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = """
+            5 2 4
+            100
+            90
+            82
+            70
+            65
+            5 2 4
+            100
+            90
+            80
+            75
+            65
+            3 1 2
+            5000
+            4000
+            3000
+            4 2 3
+            10000
+            10000
+            8000
+            8000
+            4 2 3
+            10000
+            10000
+            10000
+            8000
+            5 2 3
+            100
+            80
+            68
+            60
+            45
+            0 0 0
+        """.trimIndent()
+        val expectedOutput = """
+            3
+            4
+            2
+            2
+            3
+            2
+            
+        """.trimIndent()
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
