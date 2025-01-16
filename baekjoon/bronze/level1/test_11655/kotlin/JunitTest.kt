895db794e4a2681d9107c0846e90801eace17e16
lightaylor <115taegyeong@gmail.com>
2025-01-16 21:02:33 +0900

    feat: 11655 풀이 추가

---
+++ baekjoon/bronze/level1/test_11655/kotlin/JunitTest.kt
@@ -0,0 +1,27 @@
+package bronze.level1.test_11655.kotlin;
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = "Baekjoon Online Judge"
+        val expectedOutput = "Onrxwbba Bayvar Whqtr"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test2() {
+        val input = "One is 1"
+        val expectedOutput = "Bar vf 1"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
