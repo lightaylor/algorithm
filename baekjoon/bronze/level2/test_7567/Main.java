89e82cbe57c8ea719e8386376249e8d4d6441185
lightaylor <115taegyeong@gmail.com>
2025-01-16 20:03:57 +0900

    feat: 7567 풀이 추가

---
+++ baekjoon/bronze/level2/test_7567/Main.java
@@ -0,0 +1,16 @@
+package bronze.level2.test_7567;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr = sc.nextLine().toCharArray();
+
+        int sum = 10;
+        for (int i = 1; i < arr.length; i++) {
+            sum += arr[i - 1] == arr[i] ? 5 : 10;
+        }
+        System.out.print(sum);
+    }
+}
