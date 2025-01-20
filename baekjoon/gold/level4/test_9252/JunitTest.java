ac076824201c6806c59a9863603c81d165b0cea6
lightaylor <115taegyeong@gmail.com>
2025-01-20 21:09:34 +0900

    feat: 9252 풀이 추가

---
+++ baekjoon/gold/level4/test_9252/JunitTest.java
@@ -0,0 +1,28 @@
+package gold.level4.test_9252;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            ACAYKP
+            CAPCAK
+            """;
+        String expectedOutput = """
+            4
+            ACAK
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/gold/level4/test_9252/Main.java
@@ -0,0 +1,41 @@
+package gold.level4.test_9252;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = 1; i <= a.length(); i++) {
+            for (int j = 1; j <= b.length(); j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = a.length();
+        int j = b.length();
+        while (i > 0 && j > 0) {
+            if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                sb.append(a.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        System.out.println(dp[a.length()][b.length()]);
+        System.out.println(sb.reverse());
+    }
+}
