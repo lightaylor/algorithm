8fd4ece96f311115c5b9643980ea02579fed3aa1
lightaylor <115taegyeong@gmail.com>
2025-01-21 21:30:48 +0900

    feat: 1159 풀이 추가

---
+++ baekjoon/bronze/level2/test_1159/Main.java
@@ -0,0 +1,26 @@
+package bronze.level2.test_1159;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        Map<Character, Integer> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            char key = sc.nextLine().charAt(0);
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char key : map.keySet()) {
+            if (map.get(key) > 4) {
+                sb.append(key);
+            }
+        }
+        System.out.print(sb.length() == 0 ? "PREDAJA" : sb);
+    }
+}
