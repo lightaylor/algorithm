cdef26cb663b75a3405ba627914ca90f6f8da800
lightaylor <115taegyeong@gmail.com>
2025-01-22 21:17:01 +0900

    feat: 2902 풀이 추가

---
+++ baekjoon/bronze/level2/test_2902/JunitTest.java
@@ -0,0 +1,40 @@
+package bronze.level2.test_2902;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    
+    @Test
+    public void test_1() throws IOException {
+        String input = "Knuth-Morris-Pratt";
+        String expectedOutput = "KMP";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "Mirko-Slavko";
+        String expectedOutput = "MS";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "Pasko-Patak";
+        String expectedOutput = "PP";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level2/test_2902/Main.java
@@ -0,0 +1,17 @@
+package bronze.level2.test_2902;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split("-");
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : arr) {
+            sb.append(str.charAt(0));
+        }
+
+        System.out.print(sb);
+    }
+}
