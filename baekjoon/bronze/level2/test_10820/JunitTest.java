f2dcbc9607636c4aa7b97df4a83b21e621cd8e0f
lightaylor <115taegyeong@gmail.com>
2025-01-25 23:09:48 +0900

    feat: 10820 풀이 추가

---
+++ baekjoon/bronze/level2/test_10820/JunitTest.java
@@ -0,0 +1,31 @@
+package bronze.level2.test_10820;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            This is String
+            SPACE    1    SPACE
+             S a M p L e I n P u T    \s
+            0L1A2S3T4L5I6N7E8
+            """;
+        String expectedOutput = """
+            10 2 0 2
+            0 10 1 8
+            5 6 0 16
+            0 8 9 0
+            """;
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level2/test_10820/Main.java
@@ -0,0 +1,31 @@
+package bronze.level2.test_10820;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            char[] arr = sc.nextLine().toCharArray();
+
+            int numericCount = 0;
+            int uppercaseCount = 0;
+            int lowercaseCount = 0;
+            int spaceCount = 0;
+            for (char c : arr) {
+                if (c == 32) {
+                    spaceCount++;
+                } else if (65 <= c && c <= 90) {
+                    uppercaseCount++;
+                } else if (97 <= c && c <= 122) {
+                    lowercaseCount++;
+                } else {
+                    numericCount++;
+                }
+            }
+
+            System.out.printf("%s %s %s %s%n", lowercaseCount, uppercaseCount, numericCount, spaceCount);
+        }
+    }
+}
