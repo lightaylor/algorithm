977e4a647efa6f5e1dc4e57c9e968760b4ea7316
lightaylor <115taegyeong@gmail.com>
2025-01-26 23:03:14 +0900

    feat: 1357 풀이 추가

---
+++ baekjoon/bronze/level1/test_1357/Main.java
@@ -0,0 +1,16 @@
+package bronze.level1.test_1357;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder input = new StringBuilder(sc.nextLine()).reverse();
+        String[] arr = input.toString().split(" ");
+
+        int sum = Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]);
+        StringBuilder sb = new StringBuilder(String.valueOf(sum));
+        System.out.print(Integer.parseInt(sb.reverse().toString()));
+    }
+}
