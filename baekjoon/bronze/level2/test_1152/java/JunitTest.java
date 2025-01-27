27623181745d8a6bc782f422f81972df81faaf86
lightaylor <115taegyeong@gmail.com>
2025-01-27 21:58:37 +0900

    feat: 1152 풀이 추가

---
+++ baekjoon/bronze/level2/test_1152/java/JunitTest.java
@@ -0,0 +1,40 @@
+package bronze.level2.test_1152.java;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "The Curious Case of Benjamin Button";
+        String expectedOutput = "6";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = " The first character is a blank";
+        String expectedOutput = "6";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "The last character is a blank ";
+        String expectedOutput = "6";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level2/test_1152/kotlin/Main.kt
@@ -0,0 +1,6 @@
+package bronze.level2.test_1152.kotlin
+
+fun main() {
+    val input = readln().trim().split(" ")
+    print(input.size)
+}
