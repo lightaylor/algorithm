f593a9c291775ee9cfafc9c099b249d01c3ad4dc
lightaylor <115taegyeong@gmail.com>
2025-01-27 22:16:50 +0900

    feat: 1152 풀이 추가

--- baekjoon/bronze/level2/test_1152/kotlin/Main.kt
+++ baekjoon/bronze/level2/test_1152/kotlin/Main.kt
@@ -1,6 +1,6 @@
 package bronze.level2.test_1152.kotlin
 
 fun main() {
-    val input = readln().trim().split(" ")
+    val input = readln().split(" ").filter { it.isNotEmpty() }
     print(input.size)
 }
