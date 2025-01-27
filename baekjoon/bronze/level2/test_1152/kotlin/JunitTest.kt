69674d929dfa589b9485a8769cc321302dc06e3f
lightaylor <155070465+TaeGyeong115@users.noreply.github.com>
2025-01-27 22:17:02 +0900

    Merge pull request #600
    * feat: 1152 풀이 추가
    
    * feat: 1152 풀이 추가

---
+++ baekjoon/bronze/level2/test_1152/kotlin/JunitTest.kt
@@ -0,0 +1,36 @@
+package bronze.level2.test_1152.kotlin
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = "The Curious Case of Benjamin Button"
+        val expectedOutput = "6"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test2() {
+        val input = " The first character is a blank"
+        val expectedOutput = "6"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test3() {
+        val input = "The last character is a blank "
+        val expectedOutput = "6"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
---
+++ baekjoon/bronze/level2/test_1152/kotlin/Main.kt
@@ -0,0 +1,6 @@
+package bronze.level2.test_1152.kotlin
+
+fun main() {
+    val input = readln().split(" ").filter { it.isNotEmpty() }
+    print(input.size)
+}
