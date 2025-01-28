2fa3811625635c895599939a139f4f24c22cc633
lightaylor <155070465+TaeGyeong115@users.noreply.github.com>
2025-01-28 22:20:15 +0900

    Merge pull request #601
    * feat: 5635 풀이 추가
    
    * feat: 5635 풀이 추가

---
+++ baekjoon/silver/level5/test_5635/JunitTest.java
@@ -0,0 +1,31 @@
+package silver.level5.test_5635;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5
+            Mickey 1 10 1991
+            Alice 30 12 1990
+            Tom 15 8 1993
+            Jerry 18 9 1990
+            Garfield 20 9 1990
+            """;
+        String expectedOutput = """
+            Tom
+            Jerry
+            """;
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
