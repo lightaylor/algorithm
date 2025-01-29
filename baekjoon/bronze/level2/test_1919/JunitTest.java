1c3a95d880166b175ff6a039961e12fc254e6045
lightaylor <115taegyeong@gmail.com>
2025-01-29 19:49:13 +0900

    feat: 1919 풀이 추가

---
+++ baekjoon/bronze/level2/test_1919/JunitTest.java
@@ -0,0 +1,25 @@
+package bronze.level2.test_1919;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            aabbcc
+            xxyybb
+            """;
+        String expectedOutput = "8";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level2/test_1919/Main.java
@@ -0,0 +1,31 @@
+package bronze.level2.test_1919;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] a = sc.nextLine().toCharArray();
+        char[] b = sc.nextLine().toCharArray();
+
+        int[] aAlphabet = new int[26];
+        int[] bAlphabet = new int[26];
+
+        for (char c : a) {
+            aAlphabet[c - 'a']++;
+        }
+
+        for (char c : b) {
+            bAlphabet[c - 'a']++;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            if (aAlphabet[i] != bAlphabet[i]) {
+                sum += Math.abs(aAlphabet[i] - bAlphabet[i]);
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
