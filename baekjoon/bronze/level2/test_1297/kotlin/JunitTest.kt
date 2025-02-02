31be04cd9677208fcd1e01081aa9341f83c93d19
lightaylor <115taegyeong@gmail.com>
2025-02-02 20:07:16 +0900

    feat: 1297 풀이 추가

---
+++ baekjoon/bronze/level2/test_1297/kotlin/JunitTest.kt
@@ -0,0 +1,54 @@
+package bronze.level2.test_1297.kotlin
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = "52 9 16"
+        val expectedOutput = "25 45"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test2() {
+        val input = "7 2 3"
+        val expectedOutput = "3 5"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test3() {
+        val input = "13 7 10"
+        val expectedOutput = "7 10"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test4() {
+        val input = "7 32 47"
+        val expectedOutput = "3 5"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test5() {
+        val input = "11 15 16"
+        val expectedOutput = "7 8"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
