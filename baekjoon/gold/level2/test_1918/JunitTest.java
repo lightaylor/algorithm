08e9b2af008d916955ebb52c4cb540dcbc574dd2
lightaylor <115taegyeong@gmail.com>
2025-02-04 21:55:29 +0900

    feat: 1918 풀이 추가

---
+++ baekjoon/gold/level2/test_1918/JunitTest.java
@@ -0,0 +1,49 @@
+package gold.level2.test_1918;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "A*(B+C)";
+        String expectedOutput = "ABC+*";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "A+B";
+        String expectedOutput = "AB+";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "A+B*C";
+        String expectedOutput = "ABC*+";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = "A+B*C-D/E";
+        String expectedOutput = "ABC*+DE/-";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
