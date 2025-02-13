fc03380d2a6448ce1434c6b8d31f9103039e5616
lightaylor <115taegyeong@gmail.com>
2025-02-13 22:29:03 +0900

    feat: 11945 풀이 추가

---
+++ baekjoon/bronze/level4/test_11945/Main.java
@@ -0,0 +1,18 @@
+package bronze.level4.test_11945;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr[0]; i++) {
+            sb.setLength(0);
+            sb.append(sc.nextLine()).reverse();
+            System.out.println(sb);
+        }
+    }
+}
