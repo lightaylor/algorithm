bef342e5c08ea6f0a02c6b637402ff79b2bcadc3
lightaylor <115taegyeong@gmail.com>
2025-02-14 22:17:29 +0900

    feat: 1225 풀이 추가

---
+++ baekjoon/bronze/level2/test_1225/Main.java
@@ -0,0 +1,18 @@
+package bronze.level2.test_1225;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split(" ");
+
+        long sum = 0;
+        for (char a : arr[0].toCharArray()) {
+            for (char b : arr[1].toCharArray()) {
+                sum += (long) (a - '0') * (b - '0');
+            }
+        }
+        System.out.print(sum);
+    }
+}
