73405297317ff825608594b98a99cbd2b1a5a4af
lightaylor <115taegyeong@gmail.com>
2025-02-16 18:02:56 +0900

    feat: 1978 풀이 추가

---
+++ baekjoon/bronze/level2/test_1978/kotlin/Main.kt
@@ -0,0 +1,23 @@
+package bronze.level2.test_1978.kotlin
+
+fun main() {
+    val n = readln().toInt()
+    val arr = readln().split(" ").map { it.toInt() }
+
+    var answer = 0;
+    for (i in 0..<n) {
+        val num = arr[i]
+        var count = 0;
+
+        for (j in 2..num) {
+            if (num % j == 0) {
+                count++
+            }
+        }
+
+        if (count == 1) {
+            answer++
+        }
+    }
+    print(answer)
+}
