ef51237e11b803787cced03ecc9fc71937c364f6
lightaylor <115taegyeong@gmail.com>
2025-02-20 20:04:48 +0900

    feat: 1092 풀이 추가

---
+++ baekjoon/gold/level5/test_1092/JunitTest.java
@@ -0,0 +1,68 @@
+package gold.level5.test_1092;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            6 8 9
+            5
+            2 5 2 4 7
+            """;
+        String expectedOutput = "2";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            2
+            19 20
+            7
+            14 12 16 19 16 1 5
+            """;
+        String expectedOutput = "4";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            4
+            23 32 25 28
+            10
+            5 27 10 16 24 20 2 32 18 7
+            """;
+        String expectedOutput = "3";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+
+    @Test
+    public void test_4() throws IOException {
+        String input = """
+            10
+            11 17 5 2 20 7 5 5 20 7
+            5
+            18 18 15 15 17
+            """;
+        String expectedOutput = "2";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
