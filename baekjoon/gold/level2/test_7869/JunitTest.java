589bc66d30999be7c55e9e293286fd6cf22f3b4d
lightaylor <115taegyeong@gmail.com>
2025-03-02 18:22:16 +0900

    feat: 7869 풀이 추가

---
+++ baekjoon/gold/level2/test_7869/JunitTest.java
@@ -0,0 +1,22 @@
+package gold.level2.test_7869;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "20.0 30.0 15.0 40.0 30.0 30.0";
+        String expectedOutput = "608.366";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
