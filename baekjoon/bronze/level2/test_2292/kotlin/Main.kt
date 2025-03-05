82b49e4d361c71601a316cfd6ab6acb1faf0b85b
lightaylor <115taegyeong@gmail.com>
2025-03-05 22:15:24 +0900

    feat: 2292 풀이 추가

---
+++ baekjoon/bronze/level2/test_2292/kotlin/Main.kt
@@ -0,0 +1,14 @@
+package bronze.level2.test_2292.kotlin
+
+fun main() {
+    val n = readln().toInt()
+
+    var m = 1
+    var count = 1
+    while (m < n) {
+        m += (6 * count)
+        count++
+    }
+
+    print(count)
+}
