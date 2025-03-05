17fca1d9af129288485210e5fca1c39eeb8d7bea
lightaylor <115taegyeong@gmail.com>
2025-03-05 22:24:30 +0900

    feat: 2577 풀이 추가

---
+++ baekjoon/bronze/level2/test_2577/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package bronze.level2.test_2577.kotlin
+
+fun main() {
+    val a = readln().toInt()
+    val b = readln().toInt()
+    val c = readln().toInt()
+
+    val sum = (a * b * c).toString()
+    val arr = IntArray(10)
+    for (c in sum.toCharArray()) {
+        arr[c.digitToInt()]++
+    }
+
+    for (i in 0..9) println(arr[i])
+}
