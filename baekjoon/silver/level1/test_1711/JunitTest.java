dda2bc11eb42cc8d314bb2d12677bf00712cefac
lightaylor <115taegyeong@gmail.com>
2025-03-06 22:24:58 +0900

    feat: 1711 풀이 추가

---
+++ baekjoon/silver/level1/test_1711/JunitTest.java
@@ -0,0 +1,29 @@
+package silver.level1.test_1711;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5
+            -1 1
+            -1 0
+            0 0
+            1 0
+            1 1
+            """;
+        String expectedOutput = "7";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
