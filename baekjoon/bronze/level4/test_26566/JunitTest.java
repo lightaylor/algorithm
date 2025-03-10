f37d0dd97e6085e5677a641b6385aae0ab6c5fee
lightaylor <115taegyeong@gmail.com>
2025-03-10 21:12:24 +0900

    feat: 26566 풀이 추가

---
+++ baekjoon/bronze/level4/test_26566/JunitTest.java
@@ -0,0 +1,34 @@
+package bronze.level4.test_26566;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            8 4
+            7 9
+            9 2
+            4 7
+            841 108
+            8 606
+            """;
+        String expectedOutput = """
+            Whole pizza
+            Whole pizza
+            Slice of pizza
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
