325ec540155cedb6edf8bf977f6188048499a725
lightaylor <115taegyeong@gmail.com>
2025-03-12 19:21:46 +0900

    feat: 15803 풀이 추가

---
+++ baekjoon/silver/level5/test_15803/JunitTest.java
@@ -0,0 +1,39 @@
+package silver.level5.test_15803;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            1 1
+            1 2
+            2 1
+            """;
+        String expectedOutput = "WINNER WINNER CHICKEN DINNER!";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            12 10
+            24 20
+            36 30
+            """;
+        String expectedOutput = "WHERE IS MY CHICKEN?";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
