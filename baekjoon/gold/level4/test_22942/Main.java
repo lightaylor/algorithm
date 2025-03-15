a56ed21b6ca4b080e8d53f14cc0d86415ff524d7
lightaylor <115taegyeong@gmail.com>
2025-03-15 21:22:05 +0900

    fix: 22942 풀이

--- baekjoon/gold/level4/test_22942/Main.java
+++ baekjoon/gold/level4/test_22942/Main.java
@@ -1,40 +1,42 @@
 package gold.level4.test_22942;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         int[][] circle = new int[n][2];
         for (int i = 0; i < n; i++) {
-            circle[i][0] = sc.nextInt();
-            circle[i][1] = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            circle[i][0] = Integer.parseInt(st.nextToken());
+            circle[i][1] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(circle, Comparator.comparingInt(a -> a[0]));
-        String answer = "YES";
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                int x1 = circle[i][0];
-                int r1 = circle[i][1];
+                long x1 = circle[i][0];
+                long r1 = circle[i][1];
 
-                int x2 = circle[j][0];
-                int r2 = circle[j][1];
+                long x2 = circle[j][0];
+                long r2 = circle[j][1];
 
-                int d = Math.abs(x1 - x2);
-                if (d < r1 + r2 && d > Math.abs(r1 - r2)) {
-                    answer = "NO";
-                    break;
-                }
-
+                long d = x2 - x1;
                 if (d >= r1 + r2) break;
+                if (d > Math.abs(r1 - r2)) {
+                    System.out.print("NO");
+                    return;
+                }
             }
 
         }
-        System.out.print(answer);
+        System.out.print("YES");
     }
 }
