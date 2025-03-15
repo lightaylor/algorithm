91212b5550a89a54becf7474a7a22d7061f16ca5
lightaylor <115taegyeong@gmail.com>
2025-03-15 21:24:01 +0900

    feat: 10821 풀이

---
+++ baekjoon/bronze/level2/test_10821/Main.java
@@ -0,0 +1,11 @@
+package bronze.level2.test_10821;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split(",");
+        System.out.print(arr.length);
+    }
+}
