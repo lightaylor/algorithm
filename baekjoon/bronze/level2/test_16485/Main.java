6009fceb94fd58d26ddfe3b8428f619d26545cca
lightaylor <115taegyeong@gmail.com>
2025-03-18 19:29:29 +0900

    feat: 16485 풀이

---
+++ baekjoon/bronze/level2/test_16485/Main.java
@@ -0,0 +1,18 @@
+package bronze.level2.test_16485;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        double answer = (double) a / b;
+        if (answer == (long) answer) {
+            System.out.print((long) answer);
+        } else {
+            System.out.printf("%.10f", answer);
+        }
+    }
+}
