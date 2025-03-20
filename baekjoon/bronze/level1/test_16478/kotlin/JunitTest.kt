3359f9bbeef4e32ef239feba8d20b7630462f84a
lightaylor <115taegyeong@gmail.com>
2025-03-20 21:21:27 +0900

    feat: 16478 풀이

---
+++ baekjoon/bronze/level1/test_16478/kotlin/JunitTest.kt
@@ -0,0 +1,27 @@
+package bronze.level1.test_16478.kotlin;
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = "5 5 5"
+        val expectedOutput = "5.0"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test2() {
+        val input = "9 8 13"
+        val expectedOutput = "14.625"
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
