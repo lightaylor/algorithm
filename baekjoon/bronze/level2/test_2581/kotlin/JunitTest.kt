e2c884f284ff13a2edb0b805de8c31acf54a2959
lightaylor <115taegyeong@gmail.com>
2025-03-21 22:49:54 +0900

    feat: 2581 풀이

---
+++ baekjoon/bronze/level2/test_2581/kotlin/JunitTest.kt
@@ -0,0 +1,38 @@
+package bronze.level2.test_2581.kotlin
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import util.TestUtil
+
+class JunitTest {
+
+    @Test
+    fun test1() {
+        val input = """
+            60
+            100
+        """.trimIndent()
+        val expectedOutput = """
+            620
+            61
+        """.trimIndent()
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun test2() {
+        val input = """
+            64
+            65
+        """.trimIndent()
+        val expectedOutput = """
+            -1
+        """.trimIndent()
+
+        val actualOutput = TestUtil.executeTest(input) { main() }
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+}
