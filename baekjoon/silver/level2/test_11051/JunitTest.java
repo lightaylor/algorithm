ac20ec1e6253b2ff664439ca364557beaaf782ca
lightaylor <115taegyeong@gmail.com>
2025-03-24 20:24:12 +0900

    feat: 11051 풀이

---
+++ baekjoon/silver/level2/test_11051/JunitTest.java
@@ -0,0 +1,22 @@
+package silver.level2.test_11051;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "5 2";
+        String expectedOutput = "10";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
