2903efd8ce5e611afa9d282a9c348f4e9f2ada0e
lightaylor <115taegyeong@gmail.com>
2025-03-28 20:34:17 +0900

    feat: 18406 풀이

---
+++ baekjoon/bronze/level2/test_18406/JunitTest.java
@@ -0,0 +1,31 @@
+package bronze.level2.test_18406;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "123402";
+        String expectedOutput = "LUCKY";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "7755";
+        String expectedOutput = "READY";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level2/test_18406/Main.java
@@ -0,0 +1,19 @@
+package bronze.level2.test_18406;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] n = sc.nextLine().toCharArray();
+
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < n.length; i++) {
+            if (i < n.length / 2) left += Character.getNumericValue(n[i]);
+            else right += Character.getNumericValue(n[i]);
+        }
+
+        System.out.print(left == right ? "LUCKY" : "READY");
+    }
+}
