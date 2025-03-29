24a19e54d5af8baf713acf4269ead4c0e07da9a0
lightaylor <115taegyeong@gmail.com>
2025-03-29 23:26:46 +0900

    feat: 11328 풀이

---
+++ baekjoon/bronze/level2/test_11328/JunitTest.java
@@ -0,0 +1,33 @@
+package bronze.level2.test_11328;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            4
+            a a
+            ab ba
+            ring gnir
+            newt twan
+            """;
+        String expectedOutput = """
+            Possible
+            Possible
+            Possible
+            Impossible
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
