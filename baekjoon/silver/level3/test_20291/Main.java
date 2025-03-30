afa54f66f58acef3264bbb8fa0769326fbd08045
lightaylor <115taegyeong@gmail.com>
2025-03-30 20:10:33 +0900

    feat: 20291 풀이

---
+++ baekjoon/silver/level3/test_20291/Main.java
@@ -0,0 +1,22 @@
+package silver.level3.test_20291;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        Map<String, Integer> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] file = sc.nextLine().split("\\.");
+            map.put(file[1], map.getOrDefault(file[1], 0) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }
+    }
+}
