d78d8b06ab79e4bd06992045f875df1d0e869906
lightaylor <115taegyeong@gmail.com>
2025-04-02 23:22:31 +0900

    feat: 2607 풀이

---
+++ baekjoon/silver/level2/test_2607/Main.java
@@ -0,0 +1,30 @@
+package silver.level2.test_2607;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        String base = sc.nextLine();
+        int[] alphabet = new int[26];
+        for (char c : base.toCharArray()) alphabet[c - 'A']++;
+
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            String word = sc.nextLine();
+            int[] temp = alphabet.clone();
+            for (char c : word.toCharArray()) temp[c - 'A']--;
+
+            int plus = 0, minus = 0;
+            for (int j : temp) {
+                if (j > 0) plus += j;
+                else if (j < 0) minus += j;
+            }
+
+            if (plus < 2 && minus > -2) count++;
+        }
+        System.out.print(count);
+    }
+}
