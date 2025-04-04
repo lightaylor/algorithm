0bc05ac59b02ea7a1b9f2403507a9b1e7c5a49c1
lightaylor <115taegyeong@gmail.com>
2025-04-04 22:35:50 +0900

    feat: 15000 풀이

---
+++ baekjoon/bronze/level5/test_15000/JunitTest.java
@@ -0,0 +1,40 @@
+package bronze.level5.test_15000;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "alert";
+        String expectedOutput = "ALERT";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "earthisunderattack";
+        String expectedOutput = "EARTHISUNDERATTACK";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "canyoupickupsomegroceries";
+        String expectedOutput = "CANYOUPICKUPSOMEGROCERIES";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
