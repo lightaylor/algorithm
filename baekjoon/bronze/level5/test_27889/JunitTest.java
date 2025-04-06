be6b2b04dd8ea0c3edfbd962998598f52a6f87ec
lightaylor <115taegyeong@gmail.com>
2025-04-06 22:26:14 +0900

    feat: 27889 풀이

---
+++ baekjoon/bronze/level5/test_27889/JunitTest.java
@@ -0,0 +1,22 @@
+package bronze.level5.test_27889;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "NLCS";
+        String expectedOutput = "North London Collegiate School";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level5/test_27889/Main.java
@@ -0,0 +1,19 @@
+package bronze.level5.test_27889;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("NLCS", "North London Collegiate School");
+        map.put("BHA", "Branksome Hall Asia");
+        map.put("KIS", "Korea International School");
+        map.put("SJA", "St. Johnsbury Academy");
+
+        Scanner sc = new Scanner(System.in);
+        String key = sc.nextLine();
+        System.out.print(map.get(key));
+    }
+}
