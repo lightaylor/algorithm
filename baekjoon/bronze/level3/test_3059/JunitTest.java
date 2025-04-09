2be891ab02cdedcb25cc16c6fbe30ca7b8961033
lightaylor <115taegyeong@gmail.com>
2025-04-09 23:09:06 +0900

    feat: 3059 풀이

---
+++ baekjoon/bronze/level3/test_3059/JunitTest.java
@@ -0,0 +1,29 @@
+package bronze.level3.test_3059;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            2
+            ABCDEFGHIJKLMNOPQRSTUVW
+            A
+            """;
+        String expectedOutput = """
+            267
+            1950
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level3/test_3059/Main.java
@@ -0,0 +1,21 @@
+package bronze.level3.test_3059;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            int sum = 2015;
+            char[] arr = sc.nextLine().toCharArray();
+            Set<Character> set = new LinkedHashSet<>();
+            for (char c : arr) set.add(c);
+            for (char c : set) sum -= c;
+            System.out.println(sum);
+        }
+    }
+}
