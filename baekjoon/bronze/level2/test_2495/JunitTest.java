ef11bfc0f667c4aee8e52198b2e03077b479ee92
lightaylor <115taegyeong@gmail.com>
2025-04-11 23:34:37 +0900

    feat: 2495 풀이

---
+++ baekjoon/bronze/level2/test_2495/JunitTest.java
@@ -0,0 +1,29 @@
+package bronze.level2.test_2495;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            12345123
+            17772345
+            22233331
+            """;
+        String expectedOutput = """
+            1
+            3
+            4
+            """;
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
