26200b007b84128eb70b491978cb47c0b65f344a
lightaylor <115taegyeong@gmail.com>
2025-04-12 12:30:26 +0900

    feat: 11283 풀이

---
+++ baekjoon/bronze/level4/test_11283/JunitTest.java
@@ -0,0 +1,49 @@
+package bronze.level4.test_11283;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "가";
+        String expectedOutput = "1";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "힣";
+        String expectedOutput = "11172";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "백";
+        String expectedOutput = "4146";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = "준";
+        String expectedOutput = "7425";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
