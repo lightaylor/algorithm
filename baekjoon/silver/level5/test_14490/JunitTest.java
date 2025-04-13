232914993f54fde681d1a8c9f72cf975cd909113
lightaylor <115taegyeong@gmail.com>
2025-04-13 17:02:10 +0900

    feat: 14490 풀이

---
+++ baekjoon/silver/level5/test_14490/JunitTest.java
@@ -0,0 +1,30 @@
+package silver.level5.test_14490;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "100:10";
+        String expectedOutput = "10:1";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "18:24";
+        String expectedOutput = "3:4";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
