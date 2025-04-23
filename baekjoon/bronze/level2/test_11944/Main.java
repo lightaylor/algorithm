8dfb6a3910a382bb1e263187a78f8e7a9be61d0f
lightaylor <115taegyeong@gmail.com>
2025-04-23 23:42:37 +0900

    feat: 11944 풀이

---
+++ baekjoon/bronze/level2/test_11944/Main.java
@@ -0,0 +1,20 @@
+package bronze.level2.test_11944;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int m = Integer.parseInt(str[1]);
+        int min = Math.min(n * String.valueOf(n).length(), m);
+
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < min) {
+            sb.append(n);
+        }
+
+        System.out.print(sb.substring(0, min));
+    }
+}
