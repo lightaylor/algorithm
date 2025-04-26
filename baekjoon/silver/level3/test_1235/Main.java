2b8daba3fcfb483f18e88af6e628af095d8dbefc
lightaylor <115taegyeong@gmail.com>
2025-04-26 23:53:06 +0900

    feat: 1235 풀이

---
+++ baekjoon/silver/level3/test_1235/Main.java
@@ -0,0 +1,32 @@
+package silver.level3.test_1235;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        String[] numArr = new String[n];
+        for (int i = 0; i < n; i++) {
+            numArr[i] = sc.nextLine();
+        }
+
+        int numLength = numArr[0].length();
+        for (int i = 0; i < numLength; i++) {
+            Set<String> set = new HashSet<>();
+            for (int j = 0; j < n; j++) {
+                String str = numArr[j].substring(numLength - (i + 1));
+                if (set.contains(str)) break;
+                set.add(str);
+            }
+
+            if (set.size() == n) {
+                System.out.print(i + 1);
+                break;
+            }
+        }
+    }
+}
