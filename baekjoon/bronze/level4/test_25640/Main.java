5683aab8f1b257bb2e9231cd6930e315086615cc
lightaylor <115taegyeong@gmail.com>
2025-04-28 22:52:35 +0900

    fix: 25640 풀이

---
+++ baekjoon/bronze/level4/test_25640/Main.java
@@ -0,0 +1,17 @@
+package bronze.level4.test_25640;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String target = sc.nextLine();
+        int n = Integer.parseInt(sc.nextLine());
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            count += target.equals(sc.nextLine()) ? 1 : 0;
+        }
+        System.out.print(count);
+    }
+}
