590331854d16da3e96bbd7d5101d878d980e4c1b
lightaylor <115taegyeong@gmail.com>
2025-05-07 21:16:48 +0900

    feat: 1197 풀이

---
+++ baekjoon/gold/level4/test_1197/JunitTest.java
@@ -0,0 +1,27 @@
+package gold.level4.test_1197;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3 3
+            1 2 1
+            2 3 2
+            1 3 3
+            """;
+        String expectedOutput = "3";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
