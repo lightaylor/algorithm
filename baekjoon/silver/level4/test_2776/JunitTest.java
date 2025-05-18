ca239ab0f610e46311dd5a06979dbb88a7d5e6ca
lightaylor <115taegyeong@gmail.com>
2025-05-18 23:24:24 +0900

    feat: 2776 풀이

---
+++ baekjoon/silver/level4/test_2776/JunitTest.java
@@ -0,0 +1,34 @@
+package silver.level4.test_2776;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            1
+            5
+            4 1 5 2 3
+            5
+            1 3 7 9 5
+            """;
+        String expectedOutput = """
+            1
+            1
+            0
+            0
+            1
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level4/test_2776/Main.java
@@ -0,0 +1,29 @@
+package silver.level4.test_2776;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int m1 = Integer.parseInt(sc.nextLine());
+            int[] m1Arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Set<Integer> set = new HashSet<>(m1Arr.length);
+            for (int num : m1Arr) set.add(num);
+
+            int m2 = Integer.parseInt(sc.nextLine());
+            int[] m2Arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            for (int j : m2Arr) {
+                sb.append(set.contains(j) ? 1 : 0).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
