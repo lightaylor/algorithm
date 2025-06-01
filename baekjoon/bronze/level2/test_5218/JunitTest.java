3230b5797624a856f46ad9d1c6362d1690a81b32
lightaylor <115taegyeong@gmail.com>
2025-06-01 21:15:12 +0900

    feat: 5218 풀이

---
+++ baekjoon/bronze/level2/test_5218/JunitTest.java
@@ -0,0 +1,34 @@
+package bronze.level2.test_5218;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5
+            AAAA ABCD
+            ABCD AAAA
+            DARK LOKI
+            STRONG THANOS
+            DEADLY ULTIMO
+            """;
+        String expectedOutput = """
+            Distances: 0 1 2 3
+            Distances: 0 25 24 23
+            Distances: 8 14 19 24
+            Distances: 1 14 9 25 1 12
+            Distances: 17 7 19 5 1 16
+            """;
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level2/test_5218/Main.java
@@ -0,0 +1,20 @@
+package bronze.level2.test_5218;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder("Distances: ");
+            String[] str = sc.nextLine().split(" ");
+            for (int j = 0; j < str[0].length(); j++) {
+                int sum = str[1].charAt(j) - str[0].charAt(j);
+                sb.append(sum < 0 ? sum + 26 : sum).append(" ");
+            }
+            System.out.println(sb.toString().trim());
+        }
+    }
+}
