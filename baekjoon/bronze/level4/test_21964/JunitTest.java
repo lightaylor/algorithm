dc5fa8e11a217c9098d5e9f6db3aede931dabfe3
lightaylor <115taegyeong@gmail.com>
2025-06-14 22:47:47 +0900

    feat: 21964 풀이

---
+++ baekjoon/bronze/level4/test_21964/JunitTest.java
@@ -0,0 +1,24 @@
+package bronze.level4.test_21964;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            12
+            Sunrin,Hair.
+            """;
+        String expectedOutput = "Hair.";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
