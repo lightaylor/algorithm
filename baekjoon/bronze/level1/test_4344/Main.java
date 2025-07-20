fa123c05a8c1f02fb12840e7e6fabad825fe85ad
lightaylor <115taegyeong@gmail.com>
2025-07-20 22:03:50 +0900

    feat: 4344 풀이

---
+++ baekjoon/bronze/level1/test_4344/Main.java
@@ -0,0 +1,26 @@
+package bronze.level1.test_4344;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+
+        for (int i = 0; i < c; i++) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            float sum = 0;
+            for (int j = 0; j < n; j++) {
+                arr[j] = sc.nextInt();
+                sum += arr[j];
+            }
+
+            float avg = sum / n;
+            int count = 0;
+            for (int j : arr) if (avg < j) count++;
+            double percent = (double) count / n * 100;
+            System.out.printf("%.3f%%\n", percent);
+        }
+    }
+}
