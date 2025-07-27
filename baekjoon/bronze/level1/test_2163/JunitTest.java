21ba5a9d2863270b53ad198b971c24b3bc9608b3
lightaylor <115taegyeong@gmail.com>
2025-07-27 22:21:52 +0900

    feat: 2163 풀이

---
+++ baekjoon/bronze/level1/test_2163/JunitTest.java
@@ -0,0 +1,31 @@
+package bronze.level1.test_2163;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "2 2";
+        String expectedOutput = "3";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "1 1";
+        String expectedOutput = "0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
