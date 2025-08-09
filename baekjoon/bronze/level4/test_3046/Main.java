dc17e9d0f844c2a0956938964a14938e5b03539b
lightaylor <115taegyeong@gmail.com>
2025-08-09 16:45:57 +0900

    feat: 3046 풀이

---
+++ baekjoon/bronze/level4/test_3046/Main.java
@@ -0,0 +1,13 @@
+package bronze.level4.test_3046;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(b * 2 - a);
+    }
+}
