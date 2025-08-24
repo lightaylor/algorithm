a0afdc9f8980f434364071a75102fc48782b1c32
lightaylor <115taegyeong@gmail.com>
2025-08-24 22:13:31 +0900

    feat: 2740 풀이

---
+++ baekjoon/silver/level5/test_2740/JunitTest.java
@@ -0,0 +1,34 @@
+package silver.level5.test_2740;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3 2
+            1 2
+            3 4
+            5 6
+            2 3
+            -1 -2 0
+            0 0 3
+            """;
+        String expectedOutput = """
+            -1 -2 6
+            -3 -6 12
+            -5 -10 18
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
