26729b157e59102513f26180987bf1a026d1783d
lightaylor <115taegyeong@gmail.com>
2025-08-31 21:53:10 +0900

    feat: 9656 풀이

---
+++ baekjoon/silver/level5/test_9656/Main.java
@@ -0,0 +1,11 @@
+package silver.level5.test_9656;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n%2 == 0 ? "SK" : "CY");
+    }
+}
