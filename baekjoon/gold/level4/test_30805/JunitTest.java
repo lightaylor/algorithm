85d80205cdd1e103b9c271a332f00637040cff28
lightaylor <115taegyeong@gmail.com>
2025-09-02 20:12:07 +0900

    feat: 30805 풀이

---
+++ baekjoon/gold/level4/test_30805/JunitTest.java
@@ -0,0 +1,30 @@
+package gold.level4.test_30805;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            4
+            1 9 7 3
+            5
+            1 8 7 5 3
+            """;
+        String expectedOutput = """
+            2
+            7 3
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/gold/level4/test_30805/Main.java
@@ -0,0 +1,40 @@
+package gold.level4.test_30805;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] a = getSequence(sc);
+        int[] b = getSequence(sc);
+
+        int ia = 0, ib = 0;
+        StringBuilder ans = new StringBuilder();
+        int count = 0;
+
+        for (int v = 100; v >= 1; v--) {
+            int ta = ia, tb = ib;
+            while (true) {
+                while (ta < a.length && a[ta] != v) ta++;
+                while (tb < b.length && b[tb] != v) tb++;
+                if (ta < a.length && tb < b.length) {
+                    ans.append(v).append(' ');
+                    count++;
+                    ia = ++ta;
+                    ib = ++tb;
+                } else break;
+            }
+        }
+
+        System.out.println(count);
+        if (count > 0) System.out.println(ans.toString().trim());
+    }
+
+    private static int[] getSequence(Scanner sc) {
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        return arr;
+    }
+}
