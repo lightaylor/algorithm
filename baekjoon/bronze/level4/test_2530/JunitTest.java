40a69f68e0323a660afcb5657c5d7de6aed2b91c
lightaylor <115taegyeong@gmail.com>
2025-09-07 20:19:18 +0900

    feat: 2530 풀이

---
+++ baekjoon/bronze/level4/test_2530/JunitTest.java
@@ -0,0 +1,45 @@
+package bronze.level4.test_2530;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            14 30 0
+            200
+            """;
+        String expectedOutput = "14 33 20";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            17 40 45
+            6015
+            """;
+        String expectedOutput = "19 21 0";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            23 48 59
+            2515
+            """;
+        String expectedOutput = "0 30 54";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
