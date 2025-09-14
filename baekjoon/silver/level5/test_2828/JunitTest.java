3e48201835d6cdb7266234fe3a44f285bdc36fbd
lightaylor <115taegyeong@gmail.com>
2025-09-14 20:23:50 +0900

    feat: 2828 풀이

---
+++ baekjoon/silver/level5/test_2828/JunitTest.java
@@ -0,0 +1,43 @@
+package silver.level5.test_2828;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5 1
+            3
+            1
+            5
+            3
+            """;
+        String expectedOutput = "6";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            5 2
+            3
+            1
+            5
+            3
+            """;
+        String expectedOutput = "4";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
