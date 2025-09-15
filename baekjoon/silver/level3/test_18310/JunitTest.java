8e858b8579b77e8c766dfbfcae4b419afdb41033
lightaylor <115taegyeong@gmail.com>
2025-09-15 21:07:51 +0900

    feat: add 18310

---
+++ baekjoon/silver/level3/test_18310/JunitTest.java
@@ -0,0 +1,25 @@
+package silver.level3.test_18310;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            4
+            5 1 7 9
+            """;
+        String expectedOutput = "5";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
