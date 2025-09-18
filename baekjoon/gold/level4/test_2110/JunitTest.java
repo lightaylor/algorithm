cc236e93e0c650d894a83588a743451dad89c047
lightaylor <115taegyeong@gmail.com>
2025-09-18 20:07:35 +0900

    feat: add 2110

---
+++ baekjoon/gold/level4/test_2110/JunitTest.java
@@ -0,0 +1,29 @@
+package gold.level4.test_2110;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5 3
+            1
+            2
+            8
+            4
+            9
+            """;
+        String expectedOutput = "3";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
