476f7cb85fc117d7ede07cc7f98fd7b07087483c
lightaylor <115taegyeong@gmail.com>
2025-09-20 21:31:20 +0900

    feat: add 9237

---
+++ baekjoon/silver/level5/test_9237/JunitTest.java
@@ -0,0 +1,37 @@
+package silver.level5.test_9237;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            4
+            2 3 4 3
+            """;
+        String expectedOutput = "7";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            6
+            39 38 9 35 39 20
+            """;
+        String expectedOutput = "42";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/silver/level5/test_9237/Main.java
@@ -0,0 +1,23 @@
+package silver.level5.test_9237;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] trees = new int[n];
+        for (int i = 0; i < n; i++) trees[i] = sc.nextInt();
+        Arrays.sort(trees);
+
+        int maxDay = 0;
+        for (int i = 0; i < n; i++) {
+            int day = trees[n - 1 - i] + (i + 1);
+            maxDay = Math.max(maxDay, day);
+        }
+
+        System.out.print(maxDay + 1);
+    }
+}
