2cb577ef109f0c5e7adbde9fcae719aeb4495e04
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-10-20 09:21:11 +0900

    feat: add gold-1806 (#786)
    * feat: add 1806
    
    * feat: add JunitTest
    
    * fix: package

---
+++ baekjoon/gold/level4/test_1806/JunitTest.java
@@ -0,0 +1,25 @@
+package gold.level4.test_1806;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+          10 15
+          5 1 3 5 10 7 4 9 2 8
+          """;
+        String expectedOutput = "2";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/gold/level4/test_1806/Main.java
@@ -0,0 +1,34 @@
+package gold.level4.test_1806;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int answer = Integer.MAX_VALUE;
+        int left = 0;
+        int right = 0;
+        long sum = 0;
+
+        while (true) {
+            if (sum >= s) {
+                answer = Math.min(answer, right - left);
+                sum -= arr[left++];
+            } else if (right == n) {
+                break;
+            } else {
+                sum += arr[right++];
+            }
+        }
+
+        System.out.print(answer == Integer.MAX_VALUE ? 0 : answer);
+    }
+}
