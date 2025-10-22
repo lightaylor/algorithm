af2f8cf4d3784dd203975d195ef4a11ff240b04c
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-10-22 21:55:53 +0900

    feat: add 10942 (#787)

---
+++ baekjoon/gold/level4/test_10942/JunitTest.java
@@ -0,0 +1,34 @@
+package gold.level4.test_10942;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            7
+            1 2 1 3 1 2 1
+            4
+            1 3
+            2 5
+            3 3
+            5 7
+            """;
+        String expectedOutput = """
+            1
+            0
+            1
+            1
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
---
+++ baekjoon/gold/level4/test_10942/Main.java
@@ -0,0 +1,36 @@
+package gold.level4.test_10942;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder out = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) dp[i][i] = true;
+        for (int i = 0; i + 1 < n; i++) dp[i][i + 1] = (arr[i] == arr[i + 1]);
+        for (int len = 3; len <= n; len++) {
+            for (int s = 0; s + len - 1 < n; s++) {
+                int e = s + len - 1;
+                dp[s][e] = (arr[s] == arr[e]) && dp[s + 1][e - 1];
+            }
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end   = Integer.parseInt(st.nextToken()) - 1;
+            out.append(dp[start][end] ? 1 : 0).append('\n');
+        }
+        System.out.print(out);
+    }
+}
