7a19446e8e594d6a6b81a598b5449890c59cb943
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-10-25 20:59:17 +0900

    feat: add 2239 (#788)

---
+++ baekjoon/gold/level4/test_2239/JunitTest.java
@@ -0,0 +1,42 @@
+package gold.level4.test_2239;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            103000509
+            002109400
+            000704000
+            300502006
+            060000050
+            700803004
+            000401000
+            009205800
+            804000107
+            """;
+        String expectedOutput = """
+            143628579
+            572139468
+            986754231
+            391542786
+            468917352
+            725863914
+            237481695
+            619275843
+            854396127
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
