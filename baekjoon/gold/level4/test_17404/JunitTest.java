f81ff4a65c5bd8222a9f8cbb3d67ccbc5241a287
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-10-26 22:24:55 +0900

    feat: add 17404 (#789)

---
+++ baekjoon/gold/level4/test_17404/JunitTest.java
@@ -0,0 +1,91 @@
+package gold.level4.test_17404;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            26 40 83
+            49 60 57
+            13 89 99
+            """;
+        String expectedOutput = "110";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            3
+            1 100 100
+            100 1 100
+            100 100 1
+            """;
+        String expectedOutput = "3";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = """
+            3
+            1 100 100
+            100 100 100
+            1 100 100
+            """;
+        String expectedOutput = "201";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = """
+            6
+            30 19 5
+            64 77 64
+            15 19 97
+            4 71 57
+            90 86 84
+            93 32 91
+            """;
+        String expectedOutput = "208";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_5() throws IOException {
+        String input = """
+            8
+            71 39 44
+            32 83 55
+            51 37 63
+            89 29 100
+            83 58 11
+            65 13 15
+            47 25 29
+            60 66 19
+            """;
+        String expectedOutput = "253";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/gold/level4/test_17404/Main.java
@@ -0,0 +1,46 @@
+package gold.level4.test_17404;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    private static int[][] house;
+    private static int[][] dp;
+    private static int N;
+    private static int min;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+
+        house = new int[N][3];
+        dp = new int[N][3];
+        min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            house[i] = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        for (int startColor = 0; startColor < 3; startColor++) {
+            for (int j = 0; j < 3; j++) {
+                if (j == startColor) dp[0][j] = house[0][j];
+                else dp[0][j] = 1_001;
+            }
+
+            for (int i = 1; i < N; i++) {
+                dp[i][0] = house[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+                dp[i][1] = house[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+                dp[i][2] = house[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+            }
+
+            for (int endColor = 0; endColor < 3; endColor++) {
+                if (endColor != startColor) {
+                    min = Math.min(min, dp[N - 1][endColor]);
+                }
+            }
+        }
+
+        System.out.print(min);
+    }
+}
