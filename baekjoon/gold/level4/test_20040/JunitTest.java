7bf63fec2ed79fd90b1893e9f4c39d0bfd9809dd
lightaylor <115taegyeong@gmail.com>
2025-10-29 17:45:51 +0900

    feat: add 20040

---
+++ baekjoon/gold/level4/test_20040/JunitTest.java
@@ -0,0 +1,45 @@
+package gold.level4.test_20040;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            6 5
+            0 1
+            1 2
+            2 3
+            5 4
+            0 4
+            """;
+        String expectedOutput = "0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            6 5
+            0 1
+            1 2
+            1 3
+            0 3
+            4 5
+            """;
+        String expectedOutput = "4";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/gold/level4/test_20040/Main.java
@@ -0,0 +1,51 @@
+package gold.level4.test_20040;
+
+import java.util.Scanner;
+
+public class Main {
+    private static int[] parent, rank;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pointCount = sc.nextInt();
+        int N = sc.nextInt();
+        int answer = 0;
+
+        parent = new int[pointCount];
+        rank = new int[pointCount];
+        for (int i = 0; i < pointCount; i++) parent[i] = i;
+
+        for (int i = 0; i < N; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+
+            if (!union(from, to)) {
+                answer = i + 1;
+                break;
+            }
+        }
+        System.out.print(answer);
+    }
+
+    private static boolean union(int from, int to) {
+        int fromParent = find(from);
+        int toParent = find(to);
+
+        if (fromParent == toParent) return false;
+        if (rank[fromParent] < rank[toParent]) parent[fromParent] = toParent;
+        else if (rank[fromParent] > rank[toParent])
+            parent[toParent] = fromParent;
+        else {
+            parent[toParent] = fromParent;
+            rank[fromParent]++;
+        }
+
+        return true;
+    }
+
+    private static int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+}
+
