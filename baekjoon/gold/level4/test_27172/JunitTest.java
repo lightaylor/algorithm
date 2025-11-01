170dd880f99bbd80a28e12e777ea8ca2bd72d797
lightaylor <115taegyeong@gmail.com>
2025-11-01 15:29:16 +0900

    feat(27172): implement divisor/multiple score calculation

---
+++ baekjoon/gold/level4/test_27172/JunitTest.java
@@ -0,0 +1,37 @@
+package gold.level4.test_27172;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            3 4 12
+            """;
+        String expectedOutput = "1 1 -2";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            4
+            7 23 8 6
+            """;
+        String expectedOutput = "0 0 0 0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
