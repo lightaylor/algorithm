8458a89c0f148c976748d8f0ce273a2edef2f3cf
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-11-06 20:39:48 +0900

    feat(2143): sum of two subarray sums (#794)

---
+++ baekjoon/gold/level3/test_2143/JunitTest.java
@@ -0,0 +1,28 @@
+package gold.level3.test_2143;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            5
+            4
+            1 3 1 2
+            3
+            1 3 2
+            """;
+        String expectedOutput = "7";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
