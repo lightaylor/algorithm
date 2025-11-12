556ef99f8df67ff388b1bbf4d8f22c243a07175f
lightaylor <115taegyeong@gmail.com>
2025-11-12 19:28:29 +0900

    feat(11049): Matrix multiplication order

---
+++ baekjoon/gold/level3/test_11049/JunitTest.java
@@ -0,0 +1,27 @@
+package gold.level3.test_11049;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            5 3
+            3 2
+            2 6
+            """;
+        String expectedOutput = "90";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
