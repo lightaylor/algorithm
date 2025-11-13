57e9ead456b36b8414bb7cefe8bfad7460a19bbe
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-11-13 21:43:21 +0900

    feat(1766): Workbook (#800)

---
+++ baekjoon/gold/level2/test_1766/JunitTest.java
@@ -0,0 +1,23 @@
+package gold.level2.test_1766;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            4 2
+            4 2
+            3 1
+            """;
+        String expectedOutput = "3 1 4 2";
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
