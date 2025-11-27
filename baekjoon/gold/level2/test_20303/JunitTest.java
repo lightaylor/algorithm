45b59a6dee080f3e4aaf7c24ca8e28b8bf4c1d8e
lightaylor <115taegyeong@gmail.com>
2025-11-27 21:55:23 +0900

    feat(20303): halloween bully

---
+++ baekjoon/gold/level2/test_20303/JunitTest.java
@@ -0,0 +1,46 @@
+package gold.level2.test_20303;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            10 6 6
+            9 15 4 4 1 5 19 14 20 5
+            1 3
+            2 5
+            4 9
+            6 2
+            7 8
+            6 10
+            """;
+        String expectedOutput = "57";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = """
+            5 4 4
+            9 9 9 9 9
+            1 2
+            2 3
+            3 4
+            4 5
+            """;
+        String expectedOutput = "0";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
