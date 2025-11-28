a0547a4d18827178b1eb4c530ff3668b8118e30f
lightaylor <115taegyeong@gmail.com>
2025-11-28 22:27:25 +0900

    feat(1509): palindrome partitioning

---
+++ baekjoon/gold/level1/test_1509/JunitTest.java
@@ -0,0 +1,48 @@
+package gold.level1.test_1509;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = "BBCDDECAECBDABADDCEBACCCBDCAABDBADD";
+        String expectedOutput = "22";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        String input = "AAAA";
+        String expectedOutput = "1";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        String input = "ABCDEFGH";
+        String expectedOutput = "8";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void test_4() throws IOException {
+        String input = "QWERTYTREWQWERT";
+        String expectedOutput = "5";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
---
+++ baekjoon/gold/level1/test_1509/Main.java
@@ -0,0 +1,38 @@
+package gold.level1.test_1509;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] str = sc.nextLine().toCharArray();
+
+        int n = str.length;
+        boolean[][] palindrome = new boolean[n][n];
+        for (int i = 0; i < n; i++) palindrome[i][i] = true;
+        for (int i = 0; i < n - 1; i++)
+            palindrome[i][i + 1] = (str[i] == str[i + 1]);
+
+        for (int len = 3; len <= n; len++) {
+            for (int start = 0; start <= n - len; start++) {
+                int end = start + len - 1;
+                palindrome[start][end] = (str[start] == str[end]) && palindrome[start + 1][end - 1];
+            }
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (palindrome[j][i - 1]) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        System.out.print(dp[n]);
+    }
+}
