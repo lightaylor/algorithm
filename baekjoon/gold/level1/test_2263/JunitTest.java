845803a588c1ed939e941fbef593f2594e5c4abf
lightaylor <115taegyeong@gmail.com>
2025-12-08 20:45:42 +0900

    feat(2263): find preOrder

---
+++ baekjoon/gold/level1/test_2263/JunitTest.java
@@ -0,0 +1,26 @@
+package gold.level1.test_2263;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            1 2 3
+            1 3 2
+            """;
+        String expectedOutput = "2 1 3";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
