7de0af045fc8ab13bc4f2e3dcc99e5679d2ca187
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-12-10 20:26:44 +0900

    feat(9328): find document (#811)

---
+++ baekjoon/gold/level1/test_9328/JunitTest.java
@@ -0,0 +1,52 @@
+package gold.level1.test_9328;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            3
+            5 17
+            *****************
+            .............**$*
+            *B*A*P*C**X*Y*.X.
+            *y*x*a*p**$*$**$*
+            *****************
+            cz
+            5 11
+            *.*********
+            *...*...*x*
+            *X*.*.*.*.*
+            *$*...*...*
+            ***********
+            0
+            7 7
+            *ABCDE*
+            X.....F
+            W.$$$.G
+            V.$$$.H
+            U.$$$.J
+            T.....K
+            *SQPML*
+            irony
+            """;
+
+        String expectedOutput = """
+            3
+            1
+            0
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
