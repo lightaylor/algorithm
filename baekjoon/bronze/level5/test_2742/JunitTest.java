a0f07fd6c5602a94fe3b55578c6c30bf58ce1443
lightaylor <115taegyeong@gmail.com>
2025-12-14 20:22:29 +0900

    feat(2742): print n in reverse

---
+++ baekjoon/bronze/level5/test_2742/JunitTest.java
@@ -0,0 +1,26 @@
+package bronze.level5.test_2742;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = "5";
+        String expectedOutput = """
+            5
+            4
+            3
+            2
+            1
+            """;
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level5/test_2742/Main.java
@@ -0,0 +1,12 @@
+package bronze.level5.test_2742;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n > 0) System.out.println(n--);
+    }
+}
