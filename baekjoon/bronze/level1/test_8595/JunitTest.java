201685eeb86554233b444c464325539486bd5499
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-12-27 21:20:22 +0900

    feat(8595): hidden number (#819)

---
+++ baekjoon/bronze/level1/test_8595/JunitTest.java
@@ -0,0 +1,25 @@
+package bronze.level1.test_8595;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            14
+            ab13c9d07jeden
+            """;
+        String expectedOutput = "29";
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+}
---
+++ baekjoon/bronze/level1/test_8595/Main.java
@@ -0,0 +1,30 @@
+package bronze.level1.test_8595;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        char[] arr = sc.nextLine().toCharArray();
+
+        long sum = 0;
+        boolean isNumber = false;
+        StringBuilder sb = new StringBuilder();
+        for (char c : arr) {
+            if (Character.isDigit(c)) {
+                sb.append(c);
+                isNumber = true;
+            } else {
+                if (isNumber) {
+                    sum += Integer.parseInt(sb.toString());
+                    sb.setLength(0);
+                    isNumber = false;
+                }
+            }
+        }
+
+        if (isNumber) sum += Integer.parseInt(sb.toString());
+        System.out.print(sum);
+    }
+}
