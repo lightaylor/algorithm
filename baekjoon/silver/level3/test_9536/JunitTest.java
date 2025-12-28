3cc8f48b1cd3c231bab8e2490d8f312aac23b4b2
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2025-12-28 20:47:57 +0900

    feat(9536): What does the fox say? (#820)

---
+++ baekjoon/silver/level3/test_9536/JunitTest.java
@@ -0,0 +1,30 @@
+package silver.level3.test_9536;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static util.ThrowingRunnable.runUnchecked;
+
+public class JunitTest {
+    @Test
+    public void test_1() throws IOException {
+        String input = """
+            1
+            toot woof wa ow ow ow pa blub blub pa toot pa blub pa pa ow pow toot
+            dog goes woof
+            fish goes blub
+            elephant goes toot
+            seal goes ow
+            what does the fox say?
+            """;
+        String expectedOutput = """
+            wa pa pa pa pa pa pow
+            """;
+
+        String actualOutput = TestUtil.executeTest(input, () -> runUnchecked(() -> Main.main(new String[0])));
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
