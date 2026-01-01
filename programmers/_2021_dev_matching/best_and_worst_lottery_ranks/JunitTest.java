91ab2708d2592444d9b5cce0abfb7dc75257e1b1
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2026-01-01 16:17:31 +0900

    feat: best and worst lottery ranks (#825)

---
+++ programmers/_2021_dev_matching/best_and_worst_lottery_ranks/JunitTest.java
@@ -0,0 +1,39 @@
+package _2021_dev_matching.best_and_worst_lottery_ranks;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+public class JunitTest {
+    record Input(int[] a, int[] b) {
+    }
+
+    @Test
+    public void test_1() throws IOException {
+        int[] lottos = {44, 1, 0, 0, 31, 25};
+        int[] win_nums = {31, 10, 45, 1, 6, 19};
+        int[] expected = {3, 5};
+
+        TestUtil.assertSolution(expected, new Input(lottos, win_nums), in -> new Solution().solution(in.a(), in.b()));
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        int[] lottos = {0, 0, 0, 0, 0, 0};
+        int[] win_nums = {38, 19, 20, 40, 15, 25};
+        int[] expected = {1, 6};
+
+        TestUtil.assertSolution(expected, new Input(lottos, win_nums), in -> new Solution().solution(in.a(), in.b()));
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        int[] lottos = {45, 4, 35, 20, 3, 9};
+        int[] win_nums = {20, 9, 3, 45, 4, 35};
+        int[] expected = {1, 1};
+
+        TestUtil.assertSolution(expected, new Input(lottos, win_nums), in -> new Solution().solution(in.a(), in.b()));
+    }
+
+}
