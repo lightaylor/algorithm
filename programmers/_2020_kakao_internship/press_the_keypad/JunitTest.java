e8bf01c5b1378001c9c946bdfb77da70ff8739a9
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2026-01-03 15:54:55 +0900

    feat(67256): press the keypad (#828)

---
+++ programmers/_2020_kakao_internship/press_the_keypad/JunitTest.java
@@ -0,0 +1,49 @@
+package _2020_kakao_internship.press_the_keypad;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        String hand = "right";
+        String expected = "LRLLLRLLRRL";
+
+        TestUtil.assertSolution(
+            expected,
+            new TestUtil.Input<>(numbers, hand),
+            new Solution()::solution
+        );
+    }
+
+    @Test
+    public void test_2() throws IOException {
+        int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        String hand = "left";
+        String expected = "LRLLRRLLLRR";
+
+        TestUtil.assertSolution(
+            expected,
+            new TestUtil.Input<>(numbers, hand),
+            new Solution()::solution
+        );
+    }
+
+    @Test
+    public void test_3() throws IOException {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String hand = "right";
+        String expected = "LLRLLRLLRL";
+
+        TestUtil.assertSolution(
+            expected,
+            new TestUtil.Input<>(numbers, hand),
+            new Solution()::solution
+        );
+    }
+
+}
