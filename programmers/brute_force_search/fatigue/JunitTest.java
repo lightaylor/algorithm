e2898618081f4e8c5aeb964c1be6b8bc2e0d1e4d
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2026-01-04 16:07:50 +0900

    feat(87946): fatigue (#830)

---
+++ programmers/brute_force_search/fatigue/JunitTest.java
@@ -0,0 +1,22 @@
+package brute_force_search.fatigue;
+
+import org.junit.Test;
+import util.TestUtil;
+
+import java.io.IOException;
+
+public class JunitTest {
+
+    @Test
+    public void test_1() throws IOException {
+        int k = 80;
+        int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
+        int expected = 3;
+
+        TestUtil.assertSolution(
+            expected,
+            new TestUtil.Input<>(k, dungeons),
+            new Solution()::solution
+        );
+    }
+}
