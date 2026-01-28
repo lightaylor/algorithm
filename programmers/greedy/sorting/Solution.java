180b6d7dab6a6ba3c9de118c062bd5735f4d1183
Taegyeong Kang <155070465+lightaylor@users.noreply.github.com>
2026-01-28 18:58:46 +0900

    feat(49191): sorting (#843)

---
+++ programmers/greedy/sorting/Solution.java
@@ -0,0 +1,34 @@
+package greedy.sorting;
+
+class Solution {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+
+        boolean[][] graph = new boolean[n + 1][n + 1];
+        for (int[] result : results) {
+            graph[result[0]][result[1]] = true;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (graph[i][k] && graph[k][j]) {
+                        graph[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int count = n - 1;
+            for (int j = 1; j <= n; j++) {
+                if (i != j && (graph[i][j] || graph[j][i])) {
+                    count--;
+                }
+            }
+            if (count == 0) answer++;
+        }
+
+        return answer;
+    }
+}
