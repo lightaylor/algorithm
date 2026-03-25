92e777cf80979e953744436bdd365b025b96e687
lightaylor <115taegyeong@gmail.com>
2026-03-25 19:43:58 +0900

    feat(12987): number_game

---
+++ programmers/summer_winter_coding_2018/number_game/Solution.java
@@ -0,0 +1,23 @@
+package programmers.summer_winter_coding_2018.number_game;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int i = 0, j = 0;
+        while (i < A.length && j < B.length) {
+            if (A[i] < B[j]) {
+                answer++;
+                i++;
+            }
+            j++;
+        }
+
+        return answer;
+    }
+}
